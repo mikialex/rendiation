470fc3d0763289d016af470179842ae4d87bfcfa
mikialex <18516340862@163.com>
2020-01-12 23:45:14 +0800

    dev

---
+++ rinecraft/src/event.rs
@@ -0,0 +1,9 @@
+pub struct MouseEvent {
+  pub x: f32,
+  pub y: f32,
+}
+
+pub struct ResizeEvent {
+  pub width: f32,
+  pub height: f32,
+}
---
+++ rinecraft/src/gui/primitive.rs
@@ -0,0 +1,3 @@
+pub trait Primitive {
+    fn render();
+}  
