7d4074fc90faeec760e5b974244b442a33141619
mikialex <18516340862@163.com>
2020-02-20 23:58:42 +0800

    dev

---
+++ rendiation/src/geometry_lib/sphere_geometry.rs
@@ -0,0 +1,79 @@
+use crate::vertex::Vertex;
+use rendiation_math::*;
+
+pub trait Mesher {
+  fn create_mesh(&self) -> (Vec<Vertex>, Vec<u16>);
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct SphereGeometryParameter {
+  radius: f32,
+  width_segments: usize,
+  height_segments: usize,
+  phi_start: f32,
+  phi_length: f32,
+  theta_start: f32,
+  theta_length: f32,
+  theta_end: f32,
+}
+
+impl Mesher for SphereGeometryParameter {
+  fn create_mesh(&self) -> (Vec<Vertex>, Vec<u16>) {
+    let Self {
+      radius,
+      width_segments,
+      height_segments,
+      phi_start,
+      phi_length,
+      theta_start,
+      theta_length,
+      theta_end,
+    } = *self;
+
+    let mut index = 0;
+    let mut grid = vec![];
+
+    let mut vertices = vec![];
+    for iy in 0..height_segments {
+      let mut vertices_row = vec![];
+      let v = iy as f32 / height_segments as f32;
+      for ix in 0..width_segments {
+        let u = ix as f32 / width_segments as f32;
+        let position = Vec3::new(
+          -radius * (phi_start + u * phi_length).cos() * (theta_start + v * theta_length).sin(),
+          radius * (theta_start + v * theta_length).cos(),
+          radius * (phi_start + u * phi_length).sin() * (theta_start + v * theta_length).sin(),
+        );
+        let normal = position.normalize();
+        let uv = Vec2::new(u, 1. - v);
+        let vertex = Vertex::new(position, normal, uv);
+        vertices.push(vertex);
+        index += 1;
+        vertices_row.push(index);
+      }
+      grid.push(vertices_row);
+    }
+
+    let mut indices = vec![];
+    for iy in 0..height_segments - 1 {
+      for ix in 0..width_segments - 1 {
+        let a = grid[iy][ix + 1];
+        let b = grid[iy][ix];
+        let c = grid[iy + 1][ix];
+        let d = grid[iy + 1][ix + 1];
+        if iy != 0 || theta_start > 0. {
+          indices.push(a);
+          indices.push(b);
+          indices.push(d);
+        }
+        if iy != height_segments - 1 || theta_end < std::f32::consts::PI {
+          indices.push(b);
+          indices.push(c);
+          indices.push(d);
+        }
+      }
+    }
+
+    (vertices, indices)
+  }
+}
