d11dd41858b38df109b77ca9c32badb62b1435b4
mikialex <18516340862@163.com>
2020-02-29 14:14:40 +0800

    dev

--- rinecraft/src/shading/copy/mod.rs
+++ rinecraft/src/shading/copy/mod.rs
@@ -27,17 +27,55 @@ impl CopierShading {
   }
 }
 
-use rendiation_marco::BindGroup;
 
-#[derive(BindGroup)]
 pub struct CopyParam<'a> {
 
-  #[bind_type = "texture2d-fragment"]
   pub texture: &'a wgpu::TextureView,
   
-  #[bind_type = "sampler-fragment"]
   pub sampler: &'a WGPUSampler,
 }
+
+static mut COPY_PARAM_LAYOUT: Option<wgpu::BindGroupLayout> = None;
+
+impl<'a> BindGroupProvider for CopyParam<'a> {
+  fn provide_layout(renderer: &WGPURenderer) -> &'static wgpu::BindGroupLayout {
+    unsafe {
+      if let Some(layout) = &COPY_PARAM_LAYOUT {
+        &layout
+      } else {
+        let builder = BindGroupLayoutBuilder::new()
+          .bind_texture2d(ShaderType::Fragment)
+          .bind_sampler(ShaderType::Fragment);
+        let layout = renderer
+          .device
+          .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            bindings: &builder.bindings,
+          });
+        COPY_PARAM_LAYOUT = Some(layout);
+        COPY_PARAM_LAYOUT.as_ref().unwrap()
+      }
+    }
+  }
+
+  fn create_bindgroup(&self, renderer: &WGPURenderer) -> WGPUBindGroup {
+    BindGroupBuilder::new()
+      .texture(self.texture)
+      .sampler(self.sampler)
+      .build(&renderer.device, CopyParam::provide_layout(renderer))
+  }
+}
+
+// use rendiation_marco::BindGroup;
+
+// #[derive(BindGroup)]
+// pub struct CopyParam<'a> {
+
+//   #[bind_type = "texture2d-fragment"]
+//   pub texture: &'a wgpu::TextureView,
+  
+//   #[bind_type = "sampler-fragment"]
+//   pub sampler: &'a WGPUSampler,
+// }
 
 // static mut COPY_PARAM_LAYOUT: Option<wgpu::BindGroupLayout> = None;
 
