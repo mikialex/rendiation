dfab628abfca4ccbd13ab2b18d31cce23bf04d85
MikiAlex <18516340862@163.com>
2020-04-10 19:07:39 +0800

    dev

--- rivermesh/src/simplification/mod.rs
+++ rivermesh/src/simplification/mod.rs
@@ -1,2 +1,17 @@
 pub mod qem;
 pub mod mesh;
+
+pub struct SimplificationCtx{
+    mesh: Mesh,
+    target_face: usize,
+}
+
+impl SimplificationCtx{
+    fn decimate_edge(){
+        // remove a edge in mesh
+    }
+
+    fn simplify(){
+        
+    }
+}
