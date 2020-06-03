077abf3c4767199a069d191691fff0361e808b74
mikialex <18516340862@163.com>
2020-06-03 23:13:13 +0800

    dimension is ok?

--- math-entity/src/lib.rs
+++ math-entity/src/lib.rs
@@ -1,3 +1,6 @@
+pub mod dimension;
+pub use dimension::*;
+
 pub mod dimension3;
 pub use dimension3::*;
 pub mod dimension2;
@@ -35,59 +38,4 @@ macro_rules! intersect_reverse {
       }
     }
   };
-}
-
-use rendiation_math::*;
-
-pub trait DimensionSuccessor<T: DimensionOne<T>> {
-  type NextDimensionType;
-  fn upgrade_dimension(self, unit: T) -> Self::NextDimensionType;
-}
-
-pub trait DimensionPredecessor<T: DimensionOne<T>>{
-  type PreDimensionType;
-  fn downgrade_dimension(self) -> Self::PreDimensionType;
-}
-
-pub trait DimensionOne<T>: Copy {}
-pub trait DimensionTwo<T>: Copy {}
-// pub type DimensionTwo<T> = dyn DimensionPredecessor<T, PreDimensionType=T>;
-// pub type DimensionThree<T> = dyn DimensionPredecessor<T, PreDimensionType=DimensionTwo<T>>;
-
-impl<T: Copy> DimensionOne<T> for T {}
-
-impl<T: DimensionOne<T>> DimensionSuccessor<T> for T {
-  type NextDimensionType = Vec2<T>;
-  fn upgrade_dimension(self, unit: T) -> Self::NextDimensionType {
-    (self, unit).into()
-  }
-}
-
-impl<T: DimensionOne<T>> DimensionPredecessor<T> for Vec2<T> {
-  type PreDimensionType = T;
-  fn downgrade_dimension(self) -> Self::PreDimensionType {
-    self.x
-  }
-}
-
-impl<T: DimensionOne<T>> DimensionSuccessor<T> for Vec2<T> {
-  type NextDimensionType = Vec3<T>;
-  fn upgrade_dimension(self, unit: T) -> Self::NextDimensionType {
-    (self.x, self.y, unit).into()
-  }
-}
-
-impl<T: DimensionOne<T>> DimensionPredecessor<T> for Vec3<T> {
-  type PreDimensionType = Vec2<T>;
-  fn downgrade_dimension(self) -> Self::PreDimensionType {
-    (self.x, self.y).into()
-  }
-}
-
-#[test]
-fn test(){
-  let a = 1.upgrade_dimension(2).upgrade_dimension(3);
-  assert_eq!(a, Vec3::new(1, 2, 3));
-  assert_eq!(a.downgrade_dimension(), Vec2::new(1, 2));
-  assert_eq!(a.downgrade_dimension().downgrade_dimension(), 1);
 }
