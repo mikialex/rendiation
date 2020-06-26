6b860235dea9ef0a72f414f470c2ff1ce4ddfc39
mikialex <18516340862@163.com>
2020-06-26 18:39:44 +0800

    dev

--- utils/arena-graph/src/lib.rs
+++ utils/arena-graph/src/lib.rs
@@ -50,11 +50,62 @@ impl<T> ArenaGraph<T> {
     handle
   }
 
-  pub fn connect_node(&mut self, from: Handle<ArenaGraphNode<T>>, to: Handle<ArenaGraphNode<T>>) {
+  pub fn get_node(&self, handle: ArenaGraphNodeHandle<T>) -> &ArenaGraphNode<T> {
+    self.nodes.get(handle).unwrap()
+  }
+
+  pub fn connect_node(&mut self, from: ArenaGraphNodeHandle<T>, to: ArenaGraphNodeHandle<T>) {
     let from_node = self.nodes.get_mut(from).unwrap();
     from_node.to.insert(to);
 
     let to_node = self.nodes.get_mut(to).unwrap();
     to_node.from.insert(from);
+  }
+
+  pub fn traverse_dfs_in_topological_order(
+    &self,
+    node: ArenaGraphNodeHandle<T>,
+    visitor: &mut impl FnMut(&ArenaGraphNode<T>),
+  ) {
+    let mut unresolved: BTreeSet<ArenaGraphNodeHandle<T>> = BTreeSet::new();
+    let mut visited: BTreeSet<ArenaGraphNodeHandle<T>> = BTreeSet::new();
+
+    fn visit<T>(
+      n_handle: ArenaGraphNodeHandle<T>,
+      visited: &mut BTreeSet<ArenaGraphNodeHandle<T>>,
+      unresolved: &mut BTreeSet<ArenaGraphNodeHandle<T>>,
+      graph: &ArenaGraph<T>,
+      visitor: &mut impl FnMut(&ArenaGraphNode<T>),
+    ) {
+      if visited.contains(&n_handle) {
+        return;
+      }
+      if unresolved.contains(&n_handle) {
+        panic!("graph contains loops"); // todo
+      }
+
+      unresolved.insert(n_handle);
+
+      let node = graph.get_node(n_handle);
+      node
+        .from
+        .iter()
+        .for_each(|from_id| visit(*from_id, visited, unresolved, graph, visitor));
+
+      unresolved.remove(&n_handle);
+      visited.insert(n_handle);
+      visitor(node)
+    }
+
+    visit(node, &mut visited, &mut unresolved, self, visitor);
+  }
+
+  pub fn topological_order_list(
+    &self,
+    node: ArenaGraphNodeHandle<T>,
+  ) -> Vec<ArenaGraphNodeHandle<T>> {
+    let mut list = Vec::new();
+    self.traverse_dfs_in_topological_order(node, &mut |node| list.push(node.handle()));
+    list
   }
 }
