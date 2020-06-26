8af9c6a1946c047d1ef9703c8b189fccce72c03e
mikialex <18516340862@163.com>
2020-06-27 01:44:57 +0800

    fix compile

--- render-graph/src/lib.rs
+++ render-graph/src/lib.rs
@@ -1,60 +1,51 @@
 pub use arena_graph::*;
 pub use rendiation_math::*;
-
-
-pub fn build_graph() {
-  let mut graph = RenderGraph::new();
-  let normal_pass = graph.pass("normal").viewport();
-  let normal_target = graph.target("normal");
-  graph.screen(normal_pass);
-  // let pass = graph.pass("scene").useQuad();
-  // RenderGraph::new().root().from_pass(pass)
-}
+use std::cell::{Cell, RefCell};
 
 pub type RenderGraphNodeHandle = ArenaGraphNodeHandle<RenderGraphNode>;
-pub enum RenderGraphNode{
+pub enum RenderGraphNode {
   Pass(PassNodeData),
-  Target(TargetNodeData)
+  Target(TargetNodeData),
 }
 
 pub struct PassNodeData {
   pub name: String,
-  viewport: Vec4<f32>
+  viewport: Vec4<f32>,
 }
 
-pub struct TargetNodeData{
+pub struct TargetNodeData {
   pub name: String,
 }
 
 pub struct NodeBuilder<'a> {
   handle: RenderGraphNodeHandle,
-  graph: &'a mut RenderGraph,
+  graph: &'a RenderGraph,
 }
 
-pub struct TargetNodeBuilder<'a>{
-  builder: NodeBuilder<'a>
+pub struct TargetNodeBuilder<'a> {
+  builder: NodeBuilder<'a>,
 }
 
-pub struct PassNodeBuilder<'a>{
-  builder: NodeBuilder<'a>
+pub struct PassNodeBuilder<'a> {
+  builder: NodeBuilder<'a>,
 }
 
 impl<'a> PassNodeBuilder<'a> {
-  pub fn viewport(mut self) -> Self {
+  pub fn viewport(self) -> Self {
     self
   }
 }
 
 pub struct RenderGraph {
-  graph: ArenaGraph<RenderGraphNode>,
-  root_handle: Option<RenderGraphNodeHandle>
+  graph: RefCell<ArenaGraph<RenderGraphNode>>,
+  root_handle: Cell<Option<RenderGraphNodeHandle>>,
 }
 
 impl RenderGraph {
   pub fn new() -> Self {
     Self {
-      graph: ArenaGraph::new(),
-      root_handle: None
+      graph: RefCell::new(ArenaGraph::new()),
+      root_handle: Cell::new(None),
     }
   }
 
@@ -62,26 +53,37 @@ impl RenderGraph {
 
   pub fn render() {}
 
-  pub fn pass(&mut self, name: &str) -> PassNodeBuilder {
-    let handle = self.graph.new_node(RenderGraphNode::Pass(
-      PassNodeData {
+  pub fn pass(&self, name: &str) -> PassNodeBuilder {
+    let handle = self
+      .graph
+      .borrow_mut()
+      .new_node(RenderGraphNode::Pass(PassNodeData {
         name: name.to_owned(),
-        viewport: Vec4::zero()
-      }
-    ));
-    PassNodeBuilder{
-      builder: NodeBuilder{
+        viewport: Vec4::zero(),
+      }));
+    PassNodeBuilder {
+      builder: NodeBuilder {
         handle,
         graph: self,
-      }
+      },
     }
   }
 
-  pub fn screen(&mut self, target: PassNodeBuilder) {
-    self.root_handle = Some(target.builder.handle);
+  pub fn screen(&self, target: PassNodeBuilder) {
+    self.root_handle.set(Some(target.builder.handle));
   }
 
-  pub fn target(&mut self, name: &str) -> TargetNodeBuilder {
+  pub fn target(&self, name: &str) -> TargetNodeBuilder {
     todo!()
   }
+}
+
+pub fn build_test_graph() {
+  let mut graph = RenderGraph::new();
+  let normal_pass = graph.pass("normal").viewport();
+  let normal_target = graph.target("normal");
+  let copy_screen = graph.pass("copy_screen").viewport();
+  graph.screen(copy_screen);
+  // let pass = graph.pass("scene").useQuad();
+  // RenderGraph::new().root().from_pass(pass)
 }
