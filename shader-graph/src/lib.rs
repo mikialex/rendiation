8561f2418bd9ef4f14d21eb665a0897a5340c455
MikiAlex <18516340862@163.com>
2020-07-09 19:22:24 +0800

    update

--- shader-graph/src/lib.rs
+++ shader-graph/src/lib.rs
@@ -1,6 +1,8 @@
 use arena_graph::*;
 use std::marker::PhantomData;
 
+// static IN_BUILDING_SHADER_GRAPH: Option<ShaderGraph> = None;
+
 pub struct ShaderGraph {
   nodes: ArenaGraph<ShaderGraphNode<AnyType>>
 }
