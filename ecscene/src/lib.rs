16e3db3791b9d8443a3c5ca24e3306302073ec18
mikialex <18516340862@163.com>
2020-08-03 22:57:43 +0800

    dev

--- ecscene/src/lib.rs
+++ ecscene/src/lib.rs
@@ -5,17 +5,58 @@ pub struct Registry {
   components: HashMap<TypeId, Arena<Any>>,
 }
 
+pub enum EntityFilterKey {
+  Optional,
+  Require,
+  Refuse,
+}
+
+pub struct EntityType {
+  types: Vec<(TypeId, EntityFilterKey)>,
+}
+
 impl Registry {
-  pub fn new() -> Self {}
+  pub fn new() -> Self {
+    todo!()
+  }
+
+  pub fn iter_entity(ty: EntityType) {
+    todo!()
+  }
+}
+
+pub struct ECScene {
+  registry: Registry,
+  update_system: SceneUpdateSystem,
+  culling_system: CullingSystem,
+}
+
+pub struct WorkGraph {
+  //
 }
 
 pub fn test() {
-  let some_drawable = scene
+  let geometry = scene
     .create_entity()
     .set(index_buffer)
-    .set(vertex)
-    .set(group)
-    .set(shading);
+    .set(vertex_buffers)
+    .set(group);
+
+  let shading = scene
+    .create_entity()
+    .set(target_states)
+    .set(ras_states)
+    .set();
+
+  let shader = scene.create_entity().set();
+
+  let scene_node_root = scene.create_entity().set(transform);
+  let scene_node = scene
+    .create_entity()
+    .set(transform)
+    .set(parent(scene_node_root));
+
+  let some_drawable = scene.create_entity().set(geometry).set(shading);
 
   let some_drawable = scene.create_entity().set(geometry).set(group).set(shading);
   // let some_material = scene.create_entity()
