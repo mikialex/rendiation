a5ca05c76d5fa292c7f504d8b9c576a500fe9ba3
mikialex <18516340862@163.com>
2020-08-08 21:29:49 +0800

    fix scene webgl feature tag

--- scene-graph/src/backends/mod.rs
+++ scene-graph/src/backends/mod.rs
@@ -3,7 +3,7 @@ pub mod webgpu;
 #[cfg(feature = "webgpu")]
 pub use webgpu::*;
 
-// #[cfg(feature = "webgl")]
+#[cfg(feature = "webgl")]
 pub mod webgl;
-// #[cfg(feature = "webgl")]
+#[cfg(feature = "webgl")]
 pub use webgl::*;
