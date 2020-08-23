5901a8b6210386b02197d3cb31c8b9010e9bc417
mikialex <18516340862@163.com>
2020-08-23 11:57:19 +0800

    split header

--- shader-graph/src/code_gen/mod.rs
+++ shader-graph/src/code_gen/mod.rs
@@ -2,6 +2,7 @@ mod code_builder;
 use crate::*;
 use code_builder::CodeBuilder;
 use std::{collections::HashMap, fmt::Display};
+mod header;
 
 struct CodeGenCtx {
   var_guid: usize,
@@ -205,100 +206,5 @@ impl ShaderGraph {
     let main = builder.output();
     let lib = ctx.gen_fn_depends();
     header + "\n" + &lib + "\n" + &main
-  }
-
-  fn gen_header_vert(&self) -> String {
-    let mut result = String::from("#version 450\n");
-
-    // attributes
-    result += self
-      .attributes
-      .iter()
-      .map(|a| {
-        let info = self.nodes.get_node(a.0).data();
-        let input = info.unwrap_as_input();
-        format!(
-          "layout(location = {}) in {} {};",
-          a.1,
-          self.type_id_map.get(&info.node_type).unwrap(),
-          input.name.as_str()
-        )
-      })
-      .collect::<Vec<String>>()
-      .join("\n")
-      .as_ref();
-
-    result += self.gen_bindgroups_header(ShaderStage::Vertex).as_str();
-
-    result
-  }
-
-  fn gen_bindgroups_header(&self, stage: ShaderStage) -> String {
-    self
-      .bindgroups
-      .iter()
-      .enumerate()
-      .map(|(i, b)| b.gen_header(self, i, stage))
-      .collect::<Vec<_>>()
-      .join("\n")
-  }
-
-  fn gen_header_frag(&self) -> String {
-    let mut result = String::from("#version 450\n");
-
-    result += self.gen_bindgroups_header(ShaderStage::Fragment).as_str();
-
-    // varyings
-    result += self
-      .varyings
-      .iter()
-      .map(|a| {
-        let info = self.nodes.get_node(a.0).data();
-        // let id = info.unwrap_as_vary();
-        format!(
-          "layout(location = {}) in {} {};",
-          a.1,
-          self.type_id_map.get(&info.node_type).unwrap(),
-          format!("vary{}", a.1)
-        )
-      })
-      .collect::<Vec<String>>()
-      .join("\n")
-      .as_ref();
-
-    result
-  }
-}
-
-impl ShaderGraphBindGroup {
-  pub fn gen_header(&self, graph: &ShaderGraph, index: usize, stage: ShaderStage) -> String {
-    self
-      .inputs
-      .iter()
-      .enumerate()
-      .filter_map(|(i, h)| {
-        if stage != h.1 {
-          return None;
-        }
-        match &h.0 {
-          ShaderGraphUniformInputType::NoneUBO(node) => {
-            let info = graph.nodes.get_node(*node).data();
-            let input = info.unwrap_as_input();
-            Some(format!(
-              "layout(set = {}, binding = {}) uniform {} {};\n",
-              index,
-              i,
-              graph.type_id_map.get(&info.node_type).unwrap(),
-              input.name.as_str()
-            ))
-          }
-          ShaderGraphUniformInputType::UBO((info, _)) => Some(format!(
-            "layout(set = {}, binding = {}) {};",
-            index, i, info.code_cache
-          )),
-        }
-      })
-      .collect::<Vec<_>>()
-      .join("\n")
   }
 }
