d91e6155c8b59aeea7fb5b952e4a25a45f65556b
mikialex <18516340862@163.com>
2020-11-03 02:09:20 +0800

    dev

--- webgl/src/renderer.rs
+++ webgl/src/renderer.rs
@@ -12,6 +12,9 @@ pub struct WebGLRenderer {
   pub gl: WebGl2RenderingContext,
 
   #[wasm_bindgen(skip)]
+  pub capacities: WebGLCapabilities,
+
+  #[wasm_bindgen(skip)]
   pub attribute_states: VertexEnableStates,
 
   #[wasm_bindgen(skip)]
@@ -28,28 +31,44 @@ impl WebGLRenderer {
       .unwrap()
       .dyn_into::<WebGl2RenderingContext>()
       .unwrap();
+
+    let capacities = WebGLCapabilities::new(&gl);
+    let attribute_states = VertexEnableStates::new(capacities.max_attribute_count as usize);
+    let texture_slot_states = TextureSlotStates::new(capacities.max_combined_texture_image_units);
+
     Self {
       canvas,
       gl,
-      attribute_states: VertexEnableStates::new(10), // todo!()
-      texture_slot_states: TextureSlotStates::new(8),
+      capacities,
+      attribute_states,
+      texture_slot_states,
     }
   }
 }
 
 pub struct WebGLCapabilities {
   pub max_combined_texture_image_units: u32,
+  pub max_attribute_count: u32,
 }
 
 impl WebGLCapabilities {
-  pub fn new(_gl: &WebGl2RenderingContext) -> Self {
-    todo!()
-    // Self {
-    //   max_combined_texture_image_units: gl
-    //     .get_parameter(WebGl2RenderingContext::MAX_COMBINED_TEXTURE_IMAGE_UNITS)
-    //     .unwrap()
-    //     .dyn_into::<u32>()
-    //     .unwrap(),
-    // }
+  pub fn new(gl: &WebGl2RenderingContext) -> Self {
+    fn get_parameter_u32(gl: &WebGl2RenderingContext, parameter: u32) -> u32 {
+      gl.get_parameter(parameter)
+        .unwrap()
+        .as_f64()
+        .map(|v| v as u32)
+        // Errors will be caught by the browser or through `get_error`
+        // so return a default instead
+        .unwrap_or(0)
+    }
+
+    Self {
+      max_combined_texture_image_units: get_parameter_u32(
+        gl,
+        WebGl2RenderingContext::MAX_COMBINED_TEXTURE_IMAGE_UNITS,
+      ),
+      max_attribute_count: get_parameter_u32(gl, WebGl2RenderingContext::MAX_VERTEX_ATTRIBS),
+    }
   }
 }
