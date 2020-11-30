3d2a67414a510ad41f51a194eb0dc76344514964
mikialex <18516340862@163.com>
2020-12-01 01:55:41 +0800

    dev

--- mesh-buffer/src/tessellation/mod.rs
+++ mesh-buffer/src/tessellation/mod.rs
@@ -1,20 +1,17 @@
-use crate::vertex::Vertex;
+use crate::{geometry::IndexType, vertex::Vertex};
 
 pub mod plane;
 pub use plane::*;
 pub mod sphere;
 pub use sphere::*;
 
-pub trait IndexedBufferTessellator<T = Vertex> {
+// todo add support for index overflow check
+pub trait IndexedBufferTessellator<T = Vertex, I: IndexType = u16> {
   type TessellationParameter;
-  fn create_mesh(&self, p: &Self::TessellationParameter) -> (Vec<T>, Vec<u16>);
+  fn create_mesh(&self, p: &Self::TessellationParameter) -> (Vec<T>, Vec<I>);
 }
 
 pub trait BufferTessellator<T = Vertex> {
   type TessellationParameter;
   fn create_mesh(&self, p: &Self::TessellationParameter) -> Vec<T>;
-}
-
-pub trait IndexedGeometryBuilder<T = Vertex> {
-  fn add_vertex(&mut self, v: T) -> &mut Self;
 }
