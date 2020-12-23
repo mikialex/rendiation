ab450d48ea33ad734ef6e20752a999a1d9d38eeb
mikialex <18516340862@163.com>
2020-12-23 23:19:50 +0800

    fix

--- math/src/scalar.rs
+++ math/src/scalar.rs
@@ -1,8 +1,8 @@
 use std::fmt::Debug;
 
 use crate::Arithmetic;
-use num_traits::real::Real;
+use num_traits::Float;
 
-pub trait Scalar: Copy + Arithmetic + Real + Debug {}
+pub trait Scalar: Copy + Arithmetic + Debug + Float {}
 impl Scalar for f32 {}
 impl Scalar for f64 {}
