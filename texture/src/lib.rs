d743130fd7527e3c3e709301f28580742a2fab06
mikialex <18516340862@163.com>
2021-01-30 12:52:37 +0800

    dev

--- texture/src/lib.rs
+++ texture/src/lib.rs
@@ -2,7 +2,10 @@ pub mod address;
 pub use address::*;
 pub mod filter;
 pub use filter::*;
+use rendiation_math::Vec2;
 
-pub trait Texture {
-  // fn get()
+pub trait Texture2DContainer {
+  type Pixel;
+  fn get(&self, position: Vec2<usize>) -> &Self::Pixel;
+  fn get_mut(&mut self, position: Vec2<usize>) -> &mut Self::Pixel;
 }
