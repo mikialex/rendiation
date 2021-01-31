55dbdb5fe98ace6651ac8289cce369d9e263e94a
mikialex <18516340862@163.com>
2021-01-31 16:22:26 +0800

    dev

--- texture/src/lib.rs
+++ texture/src/lib.rs
@@ -4,8 +4,12 @@ pub mod filter;
 pub use filter::*;
 use rendiation_math::Vec2;
 
+pub use image::*;
+
 pub trait Texture2DContainer {
   type Pixel;
   fn get(&self, position: Vec2<usize>) -> &Self::Pixel;
   fn get_mut(&mut self, position: Vec2<usize>) -> &mut Self::Pixel;
 }
+
+// impl Texture2DContainer for image::ImageBuffer {}
