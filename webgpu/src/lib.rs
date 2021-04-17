b0abb8530803f33b7a52ff763f0902ad9f55769d
undefined <undefined@qunhemail.com>
2021-04-18 01:45:33 +0800

    dev

--- webgpu/src/lib.rs
+++ webgpu/src/lib.rs
@@ -187,22 +187,27 @@ pub struct VertexBuffer {
 }
 
 impl VertexBuffer {
-  pub fn new(data: impl VertexBufferSource, device: &wgpu::Device) -> Self {
+  pub fn new(data: impl VertexBufferSource) -> Self {
     let data = Box::new(data);
     Self { data, gpu: None }
   }
 
-  fn setup_pass<'a>(&mut self, renderer: &'a Renderer, pass: &mut wgpu::RenderPass<'a>, slot: u32) {
-    let gpu = self.gpu.get_or_insert_with(|| {
+  pub fn update(&mut self, renderer: &mut Renderer) {
+    let data = &self.data;
+    self.gpu.get_or_insert_with(|| {
       let device = &renderer.device;
       let gpu = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: None,
-        contents: self.data.as_bytes(),
+        contents: data.as_bytes(),
         usage: wgpu::BufferUsage::VERTEX,
       });
       renderer.buffers.insert(gpu)
     });
-    let gpu = renderer.buffers.get(*gpu).unwrap();
+  }
+
+  pub fn setup_pass<'a>(&self, renderer: &'a Renderer, pass: &mut wgpu::RenderPass<'a>, slot: u32) {
+    let gpu = self.gpu.unwrap();
+    let gpu = renderer.buffers.get(gpu).unwrap();
     pass.set_vertex_buffer(slot, gpu.slice(..));
   }
 }
@@ -223,17 +228,23 @@ impl IndexBuffer {
     let data = Box::new(data);
     Self { data, gpu: None }
   }
-  fn setup_pass<'a>(&mut self, renderer: &'a Renderer, pass: &mut wgpu::RenderPass<'a>) {
-    let gpu = self.gpu.get_or_insert_with(|| {
+
+  pub fn update(&mut self, renderer: &mut Renderer) {
+    let data = &self.data;
+    self.gpu.get_or_insert_with(|| {
       let device = &renderer.device;
       let gpu = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: None,
-        contents: self.data.as_bytes(),
+        contents: data.as_bytes(),
         usage: wgpu::BufferUsage::INDEX,
       });
       renderer.buffers.insert(gpu)
     });
-    let gpu = renderer.buffers.get(*gpu).unwrap();
+  }
+
+  pub fn setup_pass<'a>(&self, renderer: &'a Renderer, pass: &mut wgpu::RenderPass<'a>) {
+    let gpu = self.gpu.unwrap();
+    let gpu = renderer.buffers.get(gpu).unwrap();
     pass.set_index_buffer(gpu.slice(..), self.data.index_format());
   }
 }
