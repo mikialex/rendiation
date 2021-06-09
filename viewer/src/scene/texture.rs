31dfbf8c3fd67a7c5b539af4044028b7b875a3a7
mikialex <18516340862@163.com>
2021-06-09 22:49:58 +0800

    dev

--- viewer/src/scene/texture.rs
+++ viewer/src/scene/texture.rs
@@ -1,6 +1,6 @@
-use rendiation_texture::Size;
+use crate::renderer::{SceneTexture2dGpu, SceneTexture2dSource};
 
-use super::{BindableResource, Scene, Texture2DHandle};
+use super::{Scene, Texture2DHandle};
 
 pub struct SceneTexture2D {
   data: Box<dyn SceneTexture2dSource>,
@@ -9,76 +9,9 @@ pub struct SceneTexture2D {
 
 impl SceneTexture2D {
   pub fn get_gpu(&mut self, device: &wgpu::Device, queue: &wgpu::Queue) -> &SceneTexture2dGpu {
-    self.gpu.get_or_insert_with(|| {
-      let texture_extent = self.data.gpu_size();
-      let texture = device.create_texture(&wgpu::TextureDescriptor {
-        label: None,
-        size: texture_extent,
-        mip_level_count: 1,
-        sample_count: 1,
-        dimension: wgpu::TextureDimension::D2,
-        format: self.data.format(),
-        usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
-      });
-      let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-      queue.write_texture(
-        wgpu::ImageCopyTexture {
-          texture: &texture,
-          mip_level: 0,
-          origin: wgpu::Origin3d::ZERO,
-        },
-        self.data.as_bytes(),
-        wgpu::ImageDataLayout {
-          offset: 0,
-          bytes_per_row: Some(self.data.bytes_per_row()),
-          rows_per_image: None,
-        },
-        texture_extent,
-      );
-      SceneTexture2dGpu {
-        texture,
-        texture_view,
-      }
-    })
-  }
-}
-
-pub trait SceneTexture2dSource: 'static {
-  fn format(&self) -> wgpu::TextureFormat;
-  fn as_bytes(&self) -> &[u8];
-  fn size(&self) -> Size;
-  fn byte_per_pixel(&self) -> usize;
-  fn bytes_per_row(&self) -> std::num::NonZeroU32 {
-    std::num::NonZeroU32::new(self.size().width as u32 * self.byte_per_pixel() as u32).unwrap()
-  }
-  fn gpu_size(&self) -> wgpu::Extent3d {
-    let size = self.size();
-    wgpu::Extent3d {
-      width: size.width as u32,
-      height: size.height as u32,
-      depth_or_array_layers: 1,
-    }
-  }
-}
-
-impl SceneTexture2dSource for image::ImageBuffer<image::Rgba<u8>, Vec<u8>> {
-  fn format(&self) -> wgpu::TextureFormat {
-    wgpu::TextureFormat::Rgba8Unorm
-  }
-
-  fn byte_per_pixel(&self) -> usize {
-    return 4;
-  }
-
-  fn as_bytes(&self) -> &[u8] {
-    self.as_raw()
-  }
-
-  fn size(&self) -> Size {
-    Size {
-      width: self.width() as usize,
-      height: self.height() as usize,
-    }
+    self
+      .gpu
+      .get_or_insert_with(|| SceneTexture2dGpu::create(&device, queue, self.data.as_ref()))
   }
 }
 
--- viewer/src/scene/texture_cube.rs
+++ viewer/src/scene/texture_cube.rs
@@ -1,6 +1,8 @@
-use super::{BindableResource, SceneTexture2dSource};
+use crate::renderer::SceneTextureCubeGPU;
+
+use super::SceneTexture2dSource;
 
 pub struct SceneTextureCube {
-  data: Box<dyn SceneTexture2dSource>,
+  data: [Box<dyn SceneTexture2dSource>; 6],
   gpu: Option<SceneTextureCubeGPU>,
 }
