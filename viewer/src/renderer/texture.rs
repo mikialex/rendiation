39a45ffe0761313d0021730301ff1bcc79750639
mikialex <18516340862@163.com>
2021-06-10 00:54:36 +0800

    dev

--- viewer/src/renderer/texture.rs
+++ viewer/src/renderer/texture.rs
@@ -18,6 +18,14 @@ pub trait SceneTexture2dSource: 'static {
       depth_or_array_layers: 1,
     }
   }
+  fn gpu_cube_size(&self) -> wgpu::Extent3d {
+    let size = self.size();
+    wgpu::Extent3d {
+      width: size.width as u32,
+      height: size.height as u32,
+      depth_or_array_layers: 6,
+    }
+  }
 }
 
 impl SceneTexture2dSource for image::ImageBuffer<image::Rgba<u8>, Vec<u8>> {
@@ -113,5 +121,66 @@ impl BindableResource for SceneTextureCubeGPU {
       sample_type: wgpu::TextureSampleType::Float { filterable: true },
       view_dimension: wgpu::TextureViewDimension::Cube,
     }
+  }
+}
+
+impl SceneTextureCubeGPU {
+  pub fn create(
+    device: &wgpu::Device,
+    queue: &wgpu::Queue,
+    source: [&dyn SceneTexture2dSource; 6],
+  ) -> Self {
+    // check source is valid
+    let size = source[0].size();
+    let format = source[0].format();
+    assert_eq!(size.width, size.height);
+    source.iter().for_each(|s| {
+      assert_eq!(s.size(), size);
+      assert_eq!(s.format(), format);
+    });
+
+    let texture_extent = source[0].gpu_cube_size();
+    let texture = device.create_texture(&wgpu::TextureDescriptor {
+      label: None,
+      size: texture_extent,
+      mip_level_count: 1,
+      sample_count: 1,
+      dimension: wgpu::TextureDimension::D2,
+      format,
+      usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+    });
+    let texture_view = texture.create_view(&wgpu::TextureViewDescriptor {
+      label: None,
+      dimension: Some(wgpu::TextureViewDimension::Cube),
+      ..wgpu::TextureViewDescriptor::default()
+    });
+
+    todo!()
+
+    // source.iter().enumerate().for_each(|(i, source)| {
+    //   queue.write_texture(
+    //     wgpu::ImageCopyTexture {
+    //       texture: &texture,
+    //       mip_level: 0,
+    //       origin: wgpu::Origin3d {
+    //         x: 0,
+    //         y: 0,
+    //         z: i as u32,
+    //       },
+    //     },
+    //     source.as_bytes(),
+    //     wgpu::ImageDataLayout {
+    //       offset: 0,
+    //       bytes_per_row: Some(source.bytes_per_row()),
+    //       rows_per_image: None,
+    //     },
+    //     texture_extent,
+    //   );
+    // });
+
+    // SceneTextureCubeGPU {
+    //   texture,
+    //   texture_view,
+    // }
   }
 }
