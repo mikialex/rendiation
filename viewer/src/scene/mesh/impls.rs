5bf6ab5f13ba7e1a8f9f55f8cae8f9d784cdfe7a
mikialex <18516340862@163.com>
2021-06-15 01:21:47 +0800

    dev

--- viewer/src/scene/mesh/impls.rs
+++ viewer/src/scene/mesh/impls.rs
@@ -2,7 +2,7 @@ use rendiation_renderable_mesh::mesh::IndexedMesh;
 
 use super::{GPUMeshData, MeshCellGPU};
 
-impl GPUMeshData for IndexedMesh {
+impl<I, V, T, U> GPUMeshData for IndexedMesh<I, V, T, U> {
   fn update(&self, gpu: &mut Option<MeshCellGPU>, device: wgpu::Device) {
     todo!()
   }
