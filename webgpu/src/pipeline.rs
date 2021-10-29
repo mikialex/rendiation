af2e563c6a74ddcc00b20d0821fd4d954c9a8fba
mikialex <undefined@qunhemail.com>
2021-10-29 22:53:46 +0800

    dev

--- webgpu/src/pipeline.rs
+++ webgpu/src/pipeline.rs
@@ -4,4 +4,30 @@ pub struct PipelineBuilder {
 
 impl PipelineBuilder {
   //
+
+  pub fn build(device: wgpu::Device) -> wgpu::RenderPipeline {
+    device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+      label: None,
+      layout: Some(&pipeline_layout),
+      vertex: wgpu::VertexState {
+        module: &shader,
+        entry_point: "vs_main",
+        buffers: &vertex_buffers,
+      },
+      fragment: Some(wgpu::FragmentState {
+        module: &shader,
+        entry_point: "fs_main",
+        targets: targets.as_slice(),
+      }),
+      primitive: wgpu::PrimitiveState {
+        cull_mode: None,
+        topology: wgpu::PrimitiveTopology::TriangleList,
+        ..Default::default()
+      },
+      depth_stencil: self
+        .states
+        .map_depth_stencil_state(ctx.pass.depth_stencil_format),
+      multisample: wgpu::MultisampleState::default(),
+    })
+  }
 }
