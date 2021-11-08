833c22a5bff76d27503ffbf08326408709c51741
mikialex <18516340862@163.com>
2021-11-08 22:55:41 +0800

    dev

--- viewer/src/viewer/view/text_box.rs
+++ viewer/src/viewer/view/text_box.rs
@@ -1,16 +1,17 @@
 use interphaser::*;
 
 pub fn text_box(
-  placeholder: impl Into<String> + 'static,
+  placeholder: impl Into<String> + 'static + Copy,
   value: impl Into<String>,
   on_change: impl Fn(&mut String) + 'static,
 ) -> impl UIComponent<String> {
   If::condition(
     |t: &String| t.is_empty(),
-    |t| {
+    move |t| {
       Text::default()
-        // .bind(move |s, t| s.content.set(place_holder))
+        .bind(move |s, _| s.content.set(placeholder))
         .extend(Container::size((200., 80.)))
     },
   )
+  // .else_condition(todo!())
 }
