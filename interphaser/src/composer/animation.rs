88c3fd1f557a2703500a34ad607f0034e40b9470
mikialex <18516340862@163.com>
2021-12-05 12:54:36 +0800

    dev

--- interphaser/src/composer/animation.rs
+++ interphaser/src/composer/animation.rs
@@ -1,21 +1,44 @@
+use std::time::Duration;
+
 use crate::UpdateCtx;
 
 pub trait TransitionAnimator<T> {
-  fn update(&self, current: &mut T, target: &T, frame_delta: f32);
+  fn update(&self, current: &mut T, target: &T, last_frame_delta: Duration);
 }
 
 pub struct TransitionAnimation<A, T> {
   animator: A,
+  pair: Option<Pair<T>>,
+}
+
+struct Pair<T> {
   current: T,
   target: T,
 }
 
-impl<A: TransitionAnimator<T>, T> TransitionAnimation<A, T> {
+impl<A: TransitionAnimator<T>, T: Clone> TransitionAnimation<A, T> {
   pub fn update(&mut self, new: T, ctx: &mut UpdateCtx) -> &T {
-    self.target = new;
+    let Pair { current, target } = self.pair.get_or_insert_with(|| Pair {
+      current: new.clone(),
+      target: new,
+    });
+
     self
       .animator
-      .update(&mut self.current, &self.target, todo!());
-    &self.current
+      .update(current, target, ctx.last_frame_perf_info.all_time);
+
+    current
   }
+}
+
+pub enum Transition {
+  Linear,
+  Cubic {
+    //
+  },
+}
+
+pub struct TimeBasedTransition {
+  duration: Duration,
+  ty: Transition,
 }
