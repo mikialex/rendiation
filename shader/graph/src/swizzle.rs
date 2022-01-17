ca1cd2a0971bd5757c44c17dc658a125225d0f97
mikialex <undefined@qunhemail.com>
2022-01-17 19:55:38 +0800

    dev

--- shader/graph/src/swizzle.rs
+++ shader/graph/src/swizzle.rs
@@ -13,14 +13,17 @@ fn swizzle_node<I: ShaderGraphNodeType, T: ShaderGraphNodeType>(
   })
 }
 
+// improve, how to paste string literal?
 macro_rules! swizzle {
-  ($IVec: ty, $OVec: ty, $Swi: tt) => {
-    impl Node<$IVec> {
-      pub fn xyz(&self) -> Node<$OVec> {
-        swizzle_node::<_, _>(self, $Swi)
+  ($IVec: ty, $OVec: ty, $Swi: ident, $SwiTy: tt) => {
+    paste::item! {
+      impl Node<$IVec> {
+        pub fn [< $Swi >](&self) -> Node<$OVec> {
+          swizzle_node::<_, _>(self, $SwiTy)
+        }
       }
     }
   };
 }
 
-swizzle!(Vec4<f32>, Vec3<f32>, "xyz");
+swizzle!(Vec4<f32>, Vec3<f32>, xyz, "xyz");
