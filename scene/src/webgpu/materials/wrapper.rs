a824d5253f62eca132a87b93e1eeb81593e858a3
mikialex <18516340862@163.com>
2022-02-21 00:56:42 +0800

    dev

--- scene/src/webgpu/materials/wrapper.rs
+++ scene/src/webgpu/materials/wrapper.rs
@@ -21,72 +21,6 @@ pub trait IntoCommonSceneMaterial: Sized {
 
 impl<T> IntoCommonSceneMaterial for T {}
 
-impl<T: WebGPUMaterial> ShaderGraphProvider for SceneMaterialGPU<T> {}
-
-// impl<T> MaterialGPUResource for SceneMaterialGPU<T>
-// where
-//   T: MaterialGPUResource,
-// {
-//   type Source = SceneMaterial<T::Source>;
-
-//   fn create_pipeline(
-//     &self,
-//     source: &Self::Source,
-//     builder: &mut PipelineBuilder,
-//     device: &wgpu::Device,
-//     ctx: &PipelineCreateCtx,
-//   ) {
-//     source
-//       .states
-//       .apply_pipeline_builder(builder, &ctx.pass_info.format_info);
-
-//     builder.with_layout::<TransformGPU>(ctx.layouts, device);
-
-//     builder
-//       .include_vertex_entry(
-//         "
-//     [[stage(vertex)]]
-//       fn vs_main(
-//         [[location(0)]] position: vec3<f32>, // todo link with vertex type
-//         [[location(1)]] normal: vec3<f32>,
-//         [[location(2)]] uv: vec2<f32>,
-//       ) -> VertexOutput {
-//         var out: VertexOutput;
-//         out.uv = uv;
-//         out.position = camera.projection * camera.view * model.matrix * vec4<f32>(position, 1.0);;
-//         return out;
-//       }
-
-//     ",
-//       )
-//       .declare_io_struct(
-//         "
-//       struct VertexOutput {
-//         [[builtin(position)]] position: vec4<f32>;
-//         [[location(0)]] uv: vec2<f32>;
-//       };
-//     ",
-//       )
-//       .use_vertex_entry("vs_main");
-
-//     self
-//       .gpu
-//       .create_pipeline(&source.material, builder, device, ctx);
-
-//     builder.with_layout::<CameraBindgroup>(ctx.layouts, device);
-//   }
-
-//   fn setup_pass_bindgroup<'a>(
-//     &self,
-//     pass: &mut GPURenderPass<'a>,
-//     ctx: &SceneMaterialPassSetupCtx,
-//   ) {
-//     pass.set_bind_group_owned(0, &ctx.model_gpu.unwrap().bindgroup, &[]);
-//     self.gpu.setup_pass_bindgroup(pass, ctx);
-//     pass.set_bind_group_owned(2, &ctx.camera_gpu.bindgroup, &[]);
-//   }
-// }
-
 pub struct SceneMaterialGPU<T: WebGPUMaterial> {
   state_id: Cell<ValueID<MaterialStates>>,
   gpu: T::GPU,
@@ -94,15 +28,31 @@ pub struct SceneMaterialGPU<T: WebGPUMaterial> {
 
 impl<T: WebGPUMaterial> ShaderHashProvider for SceneMaterialGPU<T> {
   fn hash_pipeline(&self, hasher: &mut PipelineHasher) {
-    self.state_id.get().hash(hasher);
-    // self.gpu.hash_pipeline(hasher)
-    todo!()
+    self.state_id.get().hash(hasher); // todo where is updating
+    self.gpu.hash_pipeline(hasher);
   }
 }
 
 impl<T: WebGPUMaterial> ShaderBindingProvider for SceneMaterialGPU<T> {
   fn setup_binding(&self, builder: &mut BindingBuilder) {
-    todo!()
+    self.gpu.setup_binding(builder)
+  }
+}
+
+impl<T: WebGPUMaterial> ShaderGraphProvider for SceneMaterialGPU<T> {
+  fn build_fragment(
+    &self,
+    builder: &mut ShaderGraphFragmentBuilder,
+  ) -> Result<(), shadergraph::ShaderGraphBuildError> {
+    todo!();
+    self.gpu.build_fragment(builder)
+  }
+
+  fn build_vertex(
+    &self,
+    builder: &mut ShaderGraphVertexBuilder,
+  ) -> Result<(), ShaderGraphBuildError> {
+    self.gpu.build_vertex(builder)
   }
 }
 
--- scene/src/webgpu/rendering/utils.rs
+++ scene/src/webgpu/rendering/utils.rs
@@ -1,102 +1,70 @@
 use rendiation_algebra::Vec2;
-use rendiation_webgpu::PassTargetFormatInfo;
 use shadergraph::*;
 
 use crate::MaterialStates;
 
 static BuildVertex: &str = "
-var left: f32 = -1.0;
-var right: f32 = 1.0;
-var top: f32 = 1.0;
-var bottom: f32 = -1.0;
-var depth: f32 = 0.0;
+fn generate_quad(
+  vertex_index: u32
+) -> ? {
+  var left: f32 = -1.0;
+  var right: f32 = 1.0;
+  var top: f32 = 1.0;
+  var bottom: f32 = -1.0;
+  var depth: f32 = 0.0;
 
-switch (i32(vertex_index)) {
-  case 0: {
-    out.position = vec4<f32>(left, top, depth, 1.);
-    out.uv = vec2<f32>(0., 0.);
-  }
-  case 1: {
-    out.position = vec4<f32>(right, top, depth, 1.);
-    out.uv = vec2<f32>(1., 0.);
-  }
-  case 2: {
-    out.position = vec4<f32>(left, bottom, depth, 1.);
-    out.uv = vec2<f32>(0., 1.);
-  }
-  default: {
-    out.position = vec4<f32>(right, bottom, depth, 1.);
-    out.uv = vec2<f32>(1., 1.);
+  switch (i32(vertex_index)) {
+    case 0: {
+      out.position = vec4<f32>(left, top, depth, 1.);
+      out.uv = vec2<f32>(0., 0.);
+    }
+    case 1: {
+      out.position = vec4<f32>(right, top, depth, 1.);
+      out.uv = vec2<f32>(1., 0.);
+    }
+    case 2: {
+      out.position = vec4<f32>(left, bottom, depth, 1.);
+      out.uv = vec2<f32>(0., 1.);
+    }
+    default: {
+      out.position = vec4<f32>(right, bottom, depth, 1.);
+      out.uv = vec2<f32>(1., 1.);
+    }
   }
 }
 ";
 
-pub fn full_screen_vertex_shader(
-  builder: &mut ShaderGraphVertexBuilder,
+struct FullScreenQuad {
   blend: Option<wgpu::BlendState>,
-  format_info: &PassTargetFormatInfo,
-) {
-  builder.primitive_state = wgpu::PrimitiveState {
-    topology: wgpu::PrimitiveTopology::TriangleStrip,
-    front_face: wgpu::FrontFace::Cw,
-    ..Default::default()
-  };
+}
 
-  MaterialStates {
-    blend,
-    depth_write_enabled: false,
-    depth_compare: wgpu::CompareFunction::Always,
-    ..Default::default()
+impl ShaderGraphProvider for FullScreenQuad {
+  fn build_vertex(
+    &self,
+    _builder: &mut ShaderGraphVertexBuilder,
+  ) -> Result<(), ShaderGraphBuildError> {
+    builder.primitive_state = wgpu::PrimitiveState {
+      topology: wgpu::PrimitiveTopology::TriangleStrip,
+      front_face: wgpu::FrontFace::Cw,
+      ..Default::default()
+    };
+    todo!();
+    Ok(())
   }
-  .apply_pipeline_builder(builder, format_info);
 
-  builder
-    .declare_io_struct(
-      "
-        struct VertexOutput {
-          [[builtin(position)]] position: vec4<f32>;
-          [[location(0)]] uv: vec2<f32>;
-        };
-      ",
-    )
-    .include_vertex_entry(
-      "
-      [[stage(vertex)]]
-      fn vs_main_full_screen(
-        [[builtin(vertex_index)]] vertex_index: u32,
-      ) -> VertexOutput {
-        var out: VertexOutput;
-
-        var left: f32 = -1.0;
-        var right: f32 = 1.0;
-        var top: f32 = 1.0;
-        var bottom: f32 = -1.0;
-        var depth: f32 = 0.0;
-
-        switch (i32(vertex_index)) {
-          case 0: {
-            out.position = vec4<f32>(left, top, depth, 1.);
-            out.uv = vec2<f32>(0., 0.);
-          }
-          case 1: {
-            out.position = vec4<f32>(right, top, depth, 1.);
-            out.uv = vec2<f32>(1., 0.);
-          }
-          case 2: {
-            out.position = vec4<f32>(left, bottom, depth, 1.);
-            out.uv = vec2<f32>(0., 1.);
-          }
-          default: {
-            out.position = vec4<f32>(right, bottom, depth, 1.);
-            out.uv = vec2<f32>(1., 1.);
-          }
-        }
-
-        return out;
-      }
-    ",
-    )
-    .use_vertex_entry("vs_main_full_screen");
+  fn build_fragment(
+    &self,
+    builder: &mut ShaderGraphFragmentBuilder,
+  ) -> Result<(), ShaderGraphBuildError> {
+    MaterialStates {
+      blend: self.blend,
+      depth_write_enabled: false,
+      depth_compare: wgpu::CompareFunction::Always,
+      ..Default::default()
+    }
+    .apply_pipeline_builder(builder);
+    Ok(())
+  }
 }
 
 #[repr(C)]
