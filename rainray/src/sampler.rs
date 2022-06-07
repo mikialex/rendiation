861fc03dc852a51fe1149f333741ebf1cc0ae1ac
mikialex <18516340862@163.com>
2022-06-08 06:15:12 +0800

    investigate better sampler api

--- rainray/src/sampler.rs
+++ rainray/src/sampler.rs
@@ -10,27 +10,28 @@ use rendiation_algebra::Vec2;
 /// The task of a Sampler is to generate a sequence of -dimensional samples in
 /// [0, 1) ^ d
 pub trait Sampler {
-  fn reset(&mut self) {}
-  fn sample(&mut self) -> f32;
+  fn next(&mut self) -> f32;
 
   /// While a 2D sample value could be constructed by using values returned by a pair of calls to sample(),
   /// some samplers can generate better point distributions if they know that two dimensions will be used together.
-  fn sample_2d(&mut self) -> Vec2<f32>;
+  fn next_2d(&mut self) -> Vec2<f32>;
 
   /// For convenience, the Sampler base class provides a method that initializes a CameraSample for a given pixel.
-  fn sample_camera(&mut self, pixel: Vec2<usize>) -> CameraSample {
+  fn next_camera(&mut self, pixel: Vec2<usize>) -> CameraSample {
     CameraSample {
-      film_position: pixel.map(|v| v as f32) + self.sample_2d(),
-      time: self.sample(),
-      lens: self.sample_2d(),
+      film_position: pixel.map(|v| v as f32) + self.next_2d(),
+      time: self.next(),
+      lens: self.next_2d(),
     }
   }
 
   /// When the rendering algorithm is ready to start work on a given pixel,
   /// it starts by calling StartPixel(), providing the coordinates of the pixel in the image.
+  ///
   /// Some Sampler implementations use the knowledge of which pixel is being sampled to improve the
   /// overall distribution of the samples that they generate for the pixel, while others ignore this information.
   fn start_pixel(&mut self, _pixel: Vec2<usize>) {}
+  fn next_samples(&mut self) {}
 }
 
 pub struct CameraSample {
@@ -39,38 +40,33 @@ pub struct CameraSample {
   pub lens: Vec2<f32>,
 }
 
-/// Each storage contains samples need by one time pixel sampling
-pub struct SampleStorage {
-  samples_1d_array: Vec<f32>,
-  samples_2d_array: Vec<Vec2<f32>>,
-}
+// /// Each storage contains samples need by one time pixel sampling
+// pub struct SampleStorage {
+//   samples_1d_array: Vec<f32>,
+//   samples_2d_array: Vec<Vec2<f32>>,
+// }
 
-pub struct PixelSamplerDispatcher<T> {
-  sampler: T,
-  pixel_in_sampling: Vec2<usize>,
-  current_sample_count: usize,
-  sample_per_pixel: usize,
-  pre_computed_samples: Vec<SampleStorage>,
-}
+// pub struct PixelSamplerDispatcher<T> {
+//   sampler: T,
+//   pixel_in_sampling: Vec2<usize>,
+//   current_sample_count: usize,
+//   sample_per_pixel: usize,
+//   pre_computed_samples: Vec<SampleStorage>,
+// }
 
-impl<T> PixelSamplerDispatcher<T> {
-  pub fn prepare_samples_before_render_pixel(&mut self, sample_per_pixel: usize) {
-    self.current_sample_count = 0;
-    self.sample_per_pixel = 0;
-  }
-}
+use rand::{rngs::ThreadRng, Rng};
 
-use rand::{rngs::StdRng, Rng};
+#[derive(Default)]
 pub struct RngSampler {
-  rng: StdRng,
+  rng: ThreadRng,
 }
 
 impl Sampler for RngSampler {
-  fn sample(&mut self) -> f32 {
+  fn next(&mut self) -> f32 {
     self.rng.gen()
   }
 
-  fn sample_2d(&mut self) -> Vec2<f32> {
+  fn next_2d(&mut self) -> Vec2<f32> {
     Vec2::new(self.rng.gen(), self.rng.gen())
   }
 }
