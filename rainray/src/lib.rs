4953d5f86378cc7ae9ea2c1ec43de36747c5fdd4
mikialex <18516340862@163.com>
2022-06-09 02:47:33 +0800

    add a const sampler idea

--- rainray/src/lib.rs
+++ rainray/src/lib.rs
@@ -1,7 +1,9 @@
 #![feature(explicit_generic_args_with_impl_trait)]
+#![feature(generic_const_exprs)]
 #![allow(clippy::or_fun_call)]
 #![allow(clippy::many_single_char_names)]
 #![allow(unstable_name_collisions)]
+#![allow(incomplete_features)]
 
 mod frame;
 mod integrator;
--- rainray/src/sampler.rs
+++ rainray/src/sampler.rs
@@ -70,3 +70,34 @@ impl Sampler for RngSampler {
     Vec2::new(self.rng.gen(), self.rng.gen())
   }
 }
+
+macro_rules! AssertLeType {
+  ($left:expr, $right:expr) => {
+    [(); $right - $left]
+  };
+}
+
+macro_rules! AssertEqType {
+  ($left:expr, $right: expr) => {
+    (AssertLeType!($left, $right), AssertLeType!($right, $left))
+  };
+}
+
+/// https://github.com/rust-lang/rust/issues/76560
+/// https://hackmd.io/OZG_XiLFRs2Xmw5s39jRzA?view
+pub struct ConstSampler<const N: usize> {}
+
+impl<const N: usize> ConstSampler<N> {
+  pub fn sample<const R: usize>(self) -> ConstSampler<R>
+  where
+    AssertEqType!(N + 1, R): Sized,
+  {
+    ConstSampler {}
+  }
+}
+
+#[cfg(test)]
+pub fn test(sampler: ConstSampler<1>) -> ConstSampler<3> {
+  let sampler2 = sampler.sample::<2>();
+  sampler2.sample::<3>()
+}
