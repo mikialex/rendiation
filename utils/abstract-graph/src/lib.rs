1c70cbba317bc18ceff2f8bb8012fb5cf2f1fc10
mikialex <undefined@qunhemail.com>
2022-06-17 22:41:29 +0800

    improve absdirectedgraph

--- utils/abstract-graph/src/lib.rs
+++ utils/abstract-graph/src/lib.rs
@@ -1,50 +1,48 @@
 use std::{collections::HashSet, hash::Hash};
 
 pub trait AbstractDirectedGraph {
-  fn visit_backward(&self, visitor: impl FnMut(&Self));
-  fn visit_forward(&self, visitor: impl FnMut(&Self));
+  fn visit_backward(&self, visitor: impl FnMut(&Self) -> bool);
+  fn visit_forward(&self, visitor: impl FnMut(&Self) -> bool);
 
   /// Visit contains self node, order from the most previous one to self
-  fn traverse_dfs_in_topological_order(
-    &self,
-    visitor: &mut impl FnMut(&Self),
-    if_loop_exist: &mut impl FnMut(),
-  ) where
+  ///
+  /// Return if contains loop in graph
+  fn traverse_dfs_in_topological_order(&self, visitor: &mut impl FnMut(&Self)) -> bool
+  where
     Self: Sized + Hash + Eq + Clone,
   {
-    let mut unresolved = HashSet::new();
-    let mut visited = HashSet::new();
+    struct Ctx<T> {
+      unresolved: HashSet<T>,
+      visited: HashSet<T>,
+    }
 
     fn visit<T: AbstractDirectedGraph + Hash + Eq + Clone>(
       node: &T,
-      visited: &mut HashSet<T>,
-      unresolved: &mut HashSet<T>,
+      ctx: &mut Ctx<T>,
       visitor: &mut impl FnMut(&T),
-      if_loop_exist: &mut impl FnMut(),
     ) -> bool {
-      if visited.contains(node) {
+      if ctx.visited.contains(node) {
         return true;
       }
-      if unresolved.contains(node) {
-        if_loop_exist();
+      if ctx.unresolved.contains(node) {
         return false;
       }
 
-      unresolved.insert(node.clone());
+      ctx.unresolved.insert(node.clone());
 
-      let mut found_loop = false;
-      node.visit_backward(|from| {
-        if !found_loop {
-          found_loop = visit(from, visited, unresolved, visitor, if_loop_exist);
-        }
-      });
+      node.visit_backward(|from| visit(from, ctx, visitor));
 
-      unresolved.remove(node);
-      visited.insert(node.clone());
+      ctx.unresolved.remove(node);
+      ctx.visited.insert(node.clone());
       visitor(node);
       true
     }
 
-    visit(self, &mut visited, &mut unresolved, visitor, if_loop_exist);
+    let mut ctx = Ctx {
+      unresolved: Default::default(),
+      visited: Default::default(),
+    };
+
+    visit(self, &mut ctx, visitor)
   }
 }
