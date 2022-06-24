fa1e64941e9adcf13b36cd445b5f30bbacc0f547
mikialex <18516340862@163.com>
2022-06-24 23:54:24 +0800

    try visitor

--- shader/parser/wgsl/src/visitor.rs
+++ shader/parser/wgsl/src/visitor.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::*;
 
 pub trait ASTVisitor<T> {
@@ -12,6 +14,45 @@ impl<T, X> ASTVisitor<T> for X {
   }
 }
 
+struct ForeignImplCollector {
+  depend_user_functions: HashSet<String>,
+  depend_user_struct: HashSet<String>,
+}
+
+impl ASTVisitor<FunctionCall> for ForeignImplCollector {
+  fn visit(&mut self, ast: &FunctionCall) -> bool {
+    if !ast.is_builtin() {
+      self.depend_user_functions.insert(ast.name.name.clone());
+    }
+    true
+  }
+}
+
+impl ASTVisitor<TypeExpression> for ForeignImplCollector {
+  fn visit(&mut self, ast: &TypeExpression) -> bool {
+    if let TypeExpression::Struct(s) = ast {
+      self.depend_user_struct.insert(s.name.clone());
+    }
+    true
+  }
+}
+
+pub trait WgslASTVisitor: ASTVisitor<PrimitiveType> + ASTVisitor<TextureType> {}
+
+impl<T> WgslASTVisitor for T where T: ASTVisitor<PrimitiveType> + ASTVisitor<TextureType> {}
+
+pub trait ASTElement: Sized {
+  fn visit_children<T>(&self, _visitor: &mut T) {
+    // default don't have children
+  }
+
+  fn visit_by(&self, visitor: &mut impl ASTVisitor<Self>) {
+    if visitor.visit(self) {
+      self.visit_children(visitor)
+    }
+  }
+}
+
 struct Test;
 
 impl ASTVisitor<PrimitiveType> for Test {
@@ -20,23 +61,23 @@ impl ASTVisitor<PrimitiveType> for Test {
   }
 }
 
-pub trait WgslASTVisitor: ASTVisitor<PrimitiveType> + ASTVisitor<TextureType> {}
+#[test]
+fn t() {
+  let t: PrimitiveType = todo!();
+  t.visit_by(&mut Test)
+}
 
-impl PrimitiveType {
-  pub fn visit_by(&self, mut visitor: impl WgslASTVisitor) {
-    if visitor.visit(self) {
-      match self {
-        PrimitiveType::Scalar(v) => todo!(),
-        PrimitiveType::Vector(v) => todo!(),
-        PrimitiveType::Texture(v) => v.visit_by(visitor),
-        PrimitiveType::Sampler => {}
-      }
+impl ASTElement for PrimitiveType {
+  fn visit_children<T>(&self, visitor: &mut T) {
+    match self {
+      PrimitiveType::Scalar(v) => v.visit_by(visitor),
+      PrimitiveType::Vector(v) => v.visit_by(visitor),
+      PrimitiveType::Texture(v) => v.visit_by(visitor),
+      PrimitiveType::Sampler => {}
     }
   }
 }
 
-impl TextureType {
-  pub fn visit_by(&self, mut visitor: impl WgslASTVisitor) {
-    visitor.visit(self);
-  }
-}
+impl ASTElement for TextureType {}
+impl ASTElement for PrimitiveVectorType {}
+impl ASTElement for PrimitiveValueType {}
