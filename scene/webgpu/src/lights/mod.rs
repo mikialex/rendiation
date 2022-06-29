4f098da1ffabcc4acf7e33535a64b9bf2abf3f41
mikialex <18516340862@163.com>
2022-06-29 22:50:15 +0800

    try prototyping lights api

--- scene/webgpu/src/lights/mod.rs
+++ scene/webgpu/src/lights/mod.rs
@@ -3,9 +3,43 @@ pub use directional::*;
 use rendiation_algebra::*;
 use shadergraph::*;
 
+#[derive(Default)]
 pub struct LightList<T> {
   pub lights: Vec<T>,
 }
+
+impl<T> LightList<T> {
+  //
+}
+
+pub struct DrawDefer<'a, T> {
+  pub light: &'a T,
+}
+
+// impl<'a, T: DirectShaderLight> ShaderGraphProvider for DrawDefer<'a, T> {
+//   fn build(
+//     &self,
+//     builder: &mut ShaderGraphRenderPipelineBuilder,
+//   ) -> Result<(), ShaderGraphBuildError> {
+//     builder.fragment(|builder, binding| {
+//       // let position = builder.query::<WorldFragmentPosition>()?.get();
+//       // let normal = builder.query::<WorldFragmentNormal>()?.get();
+//       // let camera_position = builder.query::<CameraPosition>()?.get();
+
+//       // let view_dir = camera_position - position;
+
+//       // let ctx = ExpandedNode::<ShaderLightingGeometricCtx> {
+//       //   position,
+//       //   normal,
+//       //   view_dir,
+//       // };
+
+//       // let incident_light = T::compute_direct_light(todo!(), &ctx);
+
+//       Ok(())
+//     })
+//   }
+// }
 
 #[derive(Copy, Clone, ShaderStruct)]
 pub struct ShaderIncidentLight {
