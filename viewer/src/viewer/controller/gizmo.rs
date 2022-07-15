3b0c1c72bd3007178df07a1a3388e474c824e2de
mikialex <18516340862@163.com>
2022-07-15 20:34:15 +0800

    dev

--- viewer/src/viewer/controller/gizmo.rs
+++ viewer/src/viewer/controller/gizmo.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use interphaser::winit::event::Event;
+use interphaser::{winit::event::Event, MouseDown};
 use rendiation_algebra::Vec3;
 use rendiation_renderable_mesh::tessellation::{CubeMeshParameter, IndexedMeshTessellator};
 
@@ -12,12 +12,21 @@ pub struct Gizmo {
   translate: MovingGizmo,
 }
 
+impl Gizmo {
+  pub fn new(root: SceneNode) -> Self {
+    let auto_scale = ViewAutoScalable {
+      override_position: None,
+      independent_scale_factor: 100.,
+    };
+    let auto_scale = Rc::new(RefCell::new(auto_scale));
+    todo!()
+  }
+}
+
 pub struct AxisScaleGizmo {
   pub root: SceneNode,
   auto_scale: Rc<RefCell<ViewAutoScalable>>,
-  x_active: bool,
-  y_active: bool,
-  z_active: bool,
+  active: AxisActiveState,
   x: Box<dyn SceneRenderable>,
   y: Box<dyn SceneRenderable>,
   z: Box<dyn SceneRenderable>,
@@ -32,9 +41,7 @@ fn build_box() -> Box<dyn SceneRenderable> {
 pub struct RotationGizmo {
   pub root: SceneNode,
   auto_scale: Rc<RefCell<ViewAutoScalable>>,
-  x_active: bool,
-  y_active: bool,
-  z_active: bool,
+  active: AxisActiveState,
   x: Box<dyn SceneRenderable>,
   y: Box<dyn SceneRenderable>,
   z: Box<dyn SceneRenderable>,
@@ -53,16 +60,32 @@ fn build_rotation_circle() -> Box<dyn SceneRenderable> {
 pub struct MovingGizmo {
   pub root: SceneNode,
   auto_scale: Rc<RefCell<ViewAutoScalable>>,
-  x_active: bool,
-  y_active: bool,
-  z_active: bool,
+  active: AxisActiveState,
   x: Box<dyn SceneRenderable>,
   y: Box<dyn SceneRenderable>,
   z: Box<dyn SceneRenderable>,
+  xy_hint: Box<dyn SceneRenderable>,
+  xz_hint: Box<dyn SceneRenderable>,
+  zy_hint: Box<dyn SceneRenderable>,
 }
 
 fn build_axis_arrow() -> Box<dyn SceneRenderable> {
   todo!();
+}
+
+#[derive(Copy, Clone)]
+pub struct AxisActiveState {
+  x_active: bool,
+  y_active: bool,
+  z_active: bool,
+}
+
+impl AxisActiveState {
+  pub fn reset(&mut self) {
+    self.x_active = false;
+    self.y_active = false;
+    self.z_active = false;
+  }
 }
 
 impl MovingGizmo {
