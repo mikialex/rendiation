a53f19af37ebce2b6876468a25aff275417df9a9
mikialex <18516340862@163.com>
2022-12-20 22:02:46 +0800

    dev

--- scene/core/src/system.rs
+++ scene/core/src/system.rs
@@ -6,8 +6,7 @@ pub struct SceneBoundingSystem {
   /// actually data cache
   models_bounding: Vec<Box3>,
 
-  /// mesh-model reverse mapping
-  mesh_used_by_model: Arc<RwLock<HashMap<SceneMesh, MeshBoxReactiveCache>>>,
+  reactive: Arc<RwLock<Vec<Option<MeshBoxReactiveCache>>>>,
 
   update_queue: Vec<BoxUpdate>,
 
@@ -17,12 +16,59 @@ pub struct SceneBoundingSystem {
 
 struct MeshBoxReactiveCache {
   local_box_stream: Stream<Option<Box3>>,
-  models: Vec<(
-    SceneModelHandle,
-    Stream<Mat4<f32>>,
-    Stream<SceneMesh>,
-    Stream<Option<Box3>>,
-  )>,
+  world_mat_stream: Stream<Mat4<f32>>,
+  mesh_stream: Stream<SceneMesh>,
+  world_box_stream: Stream<Option<Box3>>,
+}
+
+impl MeshBoxReactiveCache {
+  pub fn from_model(model: &SceneModel) -> Option<Self> {
+    let model = model.read();
+    let world_mat_stream = model
+      .delta_stream
+      .filter_map(|node| match node.delta {
+        SceneModelImplDelta::model(_) => None,
+        SceneModelImplDelta::node(node) => Some(node),
+      })
+      .map(|node| {
+        node.visit(|node| {
+          node.delta_stream.filter_map(|d| match d.delta {
+            SceneNodeDataImplDelta::world_matrix(mat) => Some(*mat),
+            _ => None,
+          })
+        })
+      })
+      .flatten();
+
+    match &model.model {
+      SceneModelType::Standard(model) => {
+        let mesh_stream = model
+          .read()
+          .delta_stream
+          .filter_map(move |view| match view.delta {
+            StandardModelDelta::mesh(mesh_delta) => Some(mesh_delta.clone()),
+            _ => None,
+          });
+
+        // todo: we not handle mesh internal change to box change, just recompute box when mesh reference changed
+        let local_box_stream = mesh_stream.map(|mesh| mesh.read().compute_local_bound());
+
+        let world_box_stream = local_box_stream
+          .merge_map(&world_mat_stream, |local_box, world_mat| {
+            local_box.map(|b| b.apply_matrix_into(*world_mat))
+          });
+
+        MeshBoxReactiveCache {
+          local_box_stream,
+          world_mat_stream,
+          mesh_stream,
+          world_box_stream,
+        }
+        .into()
+      }
+      SceneModelType::Foreign(_) => None,
+    }
+  }
 }
 
 pub enum BoxUpdate {
@@ -37,9 +83,8 @@ impl SceneBoundingSystem {
   }
 
   pub fn new(scene: &Scene) -> Self {
-    let mesh_used_by_model: Arc<RwLock<HashMap<SceneMesh, MeshBoxReactiveCache>>> =
-      Default::default();
-    let weak_mesh_used_by_model = Arc::downgrade(&mesh_used_by_model);
+    let reactive: Arc<RwLock<Vec<Option<MeshBoxReactiveCache>>>> = Default::default();
+    let weak_reactive = Arc::downgrade(&reactive);
 
     let model_stream: Stream<&arena::ArenaDelta<SceneModel>> = scene
       .read()
@@ -50,65 +95,18 @@ impl SceneBoundingSystem {
       });
 
     model_stream.on(move |model_delta| {
-      if let Some(reactive) = weak_mesh_used_by_model.upgrade() {
-        let reactive = reactive.write().unwrap();
+      if let Some(reactive) = weak_reactive.upgrade() {
+        let mut reactive = reactive.write().unwrap();
 
         match model_delta {
-          arena::ArenaDelta::Mutate(_) => todo!(),
-          arena::ArenaDelta::Insert((model, model_handle)) => {
-            let model = model.read();
-            let world_mat_stream = model
-              .delta_stream
-              .filter_map(|node| match node.delta {
-                SceneModelImplDelta::model(_) => None,
-                SceneModelImplDelta::node(node) => Some(node),
-              })
-              .map(|node| {
-                node.visit(|node| {
-                  node.delta_stream.filter_map(|d| match d.delta {
-                    SceneNodeDataImplDelta::world_matrix(mat) => Some(*mat),
-                    _ => None,
-                  })
-                })
-              })
-              .flatten();
-
-            match &model.model {
-              SceneModelType::Standard(model) => {
-                let mesh_stream =
-                  model
-                    .read()
-                    .delta_stream
-                    .filter_map(move |view| match view.delta {
-                      StandardModelDelta::mesh(mesh_delta) => Some(mesh_delta.clone()),
-                      _ => None,
-                    });
-
-                // todo: we not handle mesh internal change to box change, just recompute box when mesh reference changed
-                let local_box_stream = mesh_stream.map(|mesh| mesh.read().compute_local_bound());
-
-                let world_box_stream = local_box_stream
-                  .merge_map(&world_mat_stream, |local_box, world_mat| {
-                    local_box.map(|b| b.apply_matrix_into(*world_mat))
-                  });
-
-                let reactive = MeshBoxReactiveCache {
-                  local_box_stream,
-                  models: vec![(
-                    *model_handle,
-                    world_mat_stream,
-                    mesh_stream,
-                    world_box_stream,
-                  )],
-                };
-
-                reactive.insert()
-              }
-              SceneModelType::Foreign(_) => todo!(),
-            }
+          arena::ArenaDelta::Mutate((new_model, handle)) => {
+            reactive[handle.into_raw_parts().0] = MeshBoxReactiveCache::from_model(new_model);
+          }
+          arena::ArenaDelta::Insert((model, handle)) => {
+            reactive[handle.into_raw_parts().0] = MeshBoxReactiveCache::from_model(model);
           }
           arena::ArenaDelta::Remove(handle) => {
-            //
+            reactive[handle.into_raw_parts().0] = None;
           }
         }
 
@@ -120,7 +118,7 @@ impl SceneBoundingSystem {
 
     Self {
       models_bounding: Default::default(),
-      mesh_used_by_model: Default::default(),
+      reactive: Default::default(),
       update_queue: Default::default(),
       bounding_change_stream: Default::default(),
     }
