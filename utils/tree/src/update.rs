21f0dc9cc7055e15139389ec3dec9b8ca353400f
mikialex <18516340862@163.com>
2023-03-19 23:25:44 +0800

    dev

--- utils/tree/src/update.rs
+++ utils/tree/src/update.rs
@@ -1,8 +1,7 @@
+use futures::{Stream, StreamExt};
 use incremental::IncrementalBase;
 
 use crate::*;
-
-// todo impl fine-grained update
 
 pub trait HierarchyDepend: Sized {
   fn update_by_parent(&mut self, parent: &Self);
@@ -21,4 +20,81 @@ impl<T: HierarchyDepend> SharedTreeCollection<T> {
 
 pub trait IncrementalHierarchyDepend: HierarchyDepend + IncrementalBase {
   fn check_delta_affects_hierarchy(delta: &Self::Delta) -> bool;
+}
+
+pub trait HierarchyDerived: Default {
+  type Source: IncrementalBase;
+  type HierarchyDirtyMark;
+
+  /// for any delta of source, check if it will have hierarchy effect
+  fn filter_hierarchy_change(
+    change: &<Self::Source as IncrementalBase>::Delta,
+  ) -> Option<Self::HierarchyDirtyMark>;
+}
+
+struct DerivedData<T: HierarchyDerived> {
+  data: T,
+  /// all sub tree change or together includes self
+  sub_tree_dirty_mark_any: T::HierarchyDirtyMark,
+
+  /// all sub tree change and together includes self
+  sub_tree_dirty_mark_all: T::HierarchyDirtyMark,
+}
+
+pub struct TreeHierarchyDerivedSystem<T: HierarchyDerived> {
+  storage: TreeCollection<DerivedData<T>>,
+}
+
+enum DirtyMarkingTask {
+  SubTree,
+}
+
+impl<T> TreeHierarchyDerivedSystem<T>
+where
+  T: HierarchyDerived,
+  T::Source: IncrementalBase,
+{
+  #[allow(unused_must_use)]
+  pub fn new(tree_delta: impl Stream<Item = SharedTreeMutation<T::Source>>) -> Self {
+    let tree = Arc::new(RwLock::new(TreeCollection::<T>::default()));
+
+    tree_delta
+      // mark stage
+      .map(|delta: SharedTreeMutation<T::Source>| {
+        let tree = tree.write().unwrap();
+        match delta {
+          SharedTreeMutation::Create(new) => {
+            // simply create the default derived. insert derived tree
+            // let derived = Default::default();
+            // tree.apply(Tree)
+          }
+          SharedTreeMutation::Delete(handle) => {
+            // do pair remove in derived tree
+          }
+          SharedTreeMutation::Mutate { node, delta } => {
+            // check if have hierarchy effect
+            // for children, do traverse mark dirty all-mark, skip if all-mark contains new dirty
+            // for parent chain, do parent chain traverse mark dirty any-mark,
+          }
+          SharedTreeMutation::Attach {
+            parent_target,
+            node,
+          } => {
+            //
+          }
+          SharedTreeMutation::Detach { node } => {
+            //
+          }
+        };
+        // do dirty marking, return if should trigger hierarchy change, and the update root
+      })
+      .buffered(500) // todo custom buffer? find common update root?
+      .map(|_| {
+        // do full tree traverse check, emit all real update as stream
+      });
+
+    Self {
+      storage: Default::default(),
+    }
+  }
 }
