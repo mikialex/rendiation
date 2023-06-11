7717eb7b788aa2bc6a68475bfcaa8cf7091c3082
mikialex <18516340862@163.com>
2023-06-11 21:22:50 +0800

    setup vulkan crates

---
+++ platform/graphics/vulkan/examples/basic.rs
@@ -0,0 +1,17 @@
+use anyhow::Result;
+use ash::Entry;
+use rendiation_vulkan::*;
+
+fn main() -> Result<()> {
+  // Vulkan instance
+  let entry = Entry::linked();
+  let vk_version = Version {
+    major: 1,
+    minor: 3,
+    ..Default::default()
+  };
+
+  let mut _instance = Instance::new(&entry, None, vk_version, "test_vk_app")?;
+
+  Ok(())
+}
