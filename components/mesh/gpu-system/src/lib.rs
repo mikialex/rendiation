199dbfccd295545443153bc712ce614e86ba3e49
mikialex <18516340862@163.com>
2023-08-31 10:42:38 +0800

    dev

--- components/mesh/gpu-system/src/lib.rs
+++ components/mesh/gpu-system/src/lib.rs
@@ -9,16 +9,25 @@ pub const MAX_STORAGE_BINDING_ARRAY_LENGTH: usize = 8192;
 pub trait GPUMeshBackend {
   type BindingCollector;
 
+  type GPUBuffer;
   type GPUIndexBuffer;
-  type GPUStorageBuffer<T>: ShaderBindingProvider<Node = ShaderReadOnlyStoragePtr<[T]>> + Clone;
-  type GPUStorageBufferBindingArray<T, const N: usize>: ShaderBindingProvider<
-      Node = ShaderHandlePtr<BindingArray<ShaderHandlePtr<ShaderReadOnlyStoragePtr<[T]>>, N>>,
-    > + Default;
+  type GPUStorageBuffer<T: ?Sized>: ShaderBindingProvider<Node = ShaderReadOnlyStoragePtr<T>>
+    + Clone;
+  type GPUStorageBufferBindingArray<T: ?Sized, const N: usize>: ShaderBindingProvider<Node = ShaderHandlePtr<BindingArray<ShaderReadOnlyStoragePtr<T>, N>>>
+    + Default;
 
   fn bind_storage<T>(collector: &mut Self::BindingCollector, sampler: &Self::GPUStorageBuffer<T>);
   fn bind_storage_array<T, const N: usize>(
     collector: &mut Self::BindingCollector,
     textures: &Self::GPUStorageBufferBindingArray<T, N>,
+  );
+
+  type DeviceQueue;
+  fn copy_from_host(
+    device_queue: &Self::DeviceQueue,
+    host: &[u8],
+    target: &Self::GPUBuffer,
+    offset: u32,
   );
 }
 
@@ -39,25 +48,35 @@ impl Drop for MeshSystemMeshInstance {
   }
 }
 
+/// could we just using the abstraction in the std like https://doc.rust-lang.org/std/alloc/trait.Allocator.html
+pub trait LinearAllocator {
+  /// return offset
+  fn allocate(&mut self, byte_size: usize) -> usize;
+  fn deallocate(&mut self, offset: usize);
+}
+
 struct MeshGPUDrivenSystemInner {}
 
 pub struct MeshGPUDrivenSystem<B: GPUMeshBackend> {
   next_id: usize,
   // range to index buffer,index to vertex_indirect_buffer
-  draw_ranges: FastHashMap<u32, Range<u32>>,
+  draw_ranges: FastHashMap<u32, (Range<u32>, DrawVertexIndirectInfo)>,
 
+  index_allocator: Box<dyn LinearAllocator>,
   index_buffer: B::GPUIndexBuffer,
+  // todo, not store here, should generate each time
+  vertex_address_buffer: B::GPUStorageBuffer<[DrawVertexIndirectInfo]>,
 
-  position_vertex_buffers: Slab<B::GPUStorageBuffer<Vec3<f32>>>,
-  normal_vertex_buffers: Slab<B::GPUStorageBuffer<Vec3<f32>>>,
-  normal_uv_buffers: Slab<B::GPUStorageBuffer<Vec2<f32>>>,
+  position_vertex_buffers: Slab<B::GPUStorageBuffer<[Vec3<f32>]>>,
+  normal_vertex_buffers: Slab<B::GPUStorageBuffer<[Vec3<f32>]>>,
+  normal_uv_buffers: Slab<B::GPUStorageBuffer<[Vec2<f32>]>>,
 
   bindless_position_vertex_buffers_f32:
-    B::GPUStorageBufferBindingArray<Vec3<f32>, MAX_STORAGE_BINDING_ARRAY_LENGTH>,
+    B::GPUStorageBufferBindingArray<[Vec3<f32>], MAX_STORAGE_BINDING_ARRAY_LENGTH>,
   bindless_normal_vertex_buffers_u32:
-    B::GPUStorageBufferBindingArray<Vec3<f32>, MAX_STORAGE_BINDING_ARRAY_LENGTH>,
+    B::GPUStorageBufferBindingArray<[Vec3<f32>], MAX_STORAGE_BINDING_ARRAY_LENGTH>,
   bindless_uv_vertex_buffers_u32:
-    B::GPUStorageBufferBindingArray<Vec2<f32>, MAX_STORAGE_BINDING_ARRAY_LENGTH>,
+    B::GPUStorageBufferBindingArray<[Vec2<f32>], MAX_STORAGE_BINDING_ARRAY_LENGTH>,
 }
 
 // impl Stream for MeshGPUDrivenSystem {
@@ -97,6 +116,10 @@ pub struct DrawIndirect {
 }
 
 impl<B: GPUMeshBackend> MeshGPUDrivenSystem<B> {
+  pub fn maintain(&mut self) {
+    // todo check any changed
+  }
+
   pub fn create_mesh_instance(
     &mut self,
     position: Vec<f32>,
@@ -108,26 +131,59 @@ impl<B: GPUMeshBackend> MeshGPUDrivenSystem<B> {
 
   pub fn generate_draw_command_buffer_from_host(
     &self,
-    buffer: impl Iterator<Item = MeshSystemMeshHandle>,
-  ) -> impl Iterator<Item = (DrawIndirect, DrawVertexIndirectInfo)> {
-    [].into_iter() // todo
+    buffer: impl Iterator<Item = MeshSystemMeshHandle> + 'static,
+  ) -> impl Iterator<Item = (DrawIndirect, DrawVertexIndirectInfo)> + '_ {
+    buffer.enumerate().map(|(i, handle)| {
+      let (range, vertex_info) = self.draw_ranges.get(&handle.inner).unwrap();
+      let draw_indirect = DrawIndirect {
+        vertex_count: range.end - range.start,
+        instance_count: 1,
+        base_vertex: range.start,
+        base_instance: i as u32, // we rely on this to get draw id. https://www.g-truc.net/post-0518.html
+      };
+      (draw_indirect, *vertex_info)
+    })
   }
 
-  /// user could use this in their compute shader to generate the buffer we want
-  pub fn generate_draw_command_on_device(
-    &self,
-    handle: Node<u32>,
-  ) -> (Node<DrawIndirect>, Node<DrawVertexIndirectInfo>) {
-    todo!()
-  }
+  // /// user could use this in their compute shader to generate the buffer we want
+  // pub fn generate_draw_command_on_device(
+  //   &self,
+  //   handle: Node<u32>,
+  // ) -> (Node<DrawIndirect>, Node<DrawVertexIndirectInfo>) {
+  //   todo!()
+  // }
 
-  // todo, check how to get draw_id https://www.g-truc.net/post-0518.html
   pub fn fetch_vertex_on_device(
     &self,
-    draw_id: Node<u32>,
-    vertex_id: Node<u32>,
-    info: Node<DrawVertexIndirectInfo>,
-  ) -> Node<Vertex> {
-    todo!()
+    vertex: ShaderVertexBuilder,
+    mut binding: ShaderBindGroupDirectBuilder,
+  ) -> ENode<Vertex> {
+    let draw_id = vertex.query::<VertexInstanceIndex>().unwrap();
+    let vertex_id = vertex.query::<VertexIndex>().unwrap();
+
+    let vertex_addresses = binding.bind_by(&self.vertex_address_buffer);
+    let vertex_address = vertex_addresses.index(draw_id).load().expand();
+
+    let position = binding.bind_by(&self.bindless_position_vertex_buffers_f32);
+    let position = position.index(vertex_address.position_buffer_id);
+    let position = position
+      .index(vertex_address.position_buffer_offset + vertex_id)
+      .load();
+
+    let normal = binding.bind_by(&self.bindless_normal_vertex_buffers_u32);
+    let normal = normal.index(vertex_address.position_buffer_id);
+    let normal = normal
+      .index(vertex_address.normal_buffer_offset + vertex_id)
+      .load();
+
+    let uv = binding.bind_by(&self.bindless_uv_vertex_buffers_u32);
+    let uv = uv.index(vertex_address.position_buffer_id);
+    let uv = uv.index(vertex_address.uv_buffer_offset + vertex_id).load();
+
+    ENode::<Vertex> {
+      position,
+      normal,
+      uv,
+    }
   }
 }
