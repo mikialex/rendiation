b4321378723b1cce9e7064169fc853942fc5e488
mikialex <18516340862@163.com>
2024-08-16 16:12:52 +0800

    improve

--- shader/ray-tracing/src/api/backend.rs
+++ shader/ray-tracing/src/api/backend.rs
@@ -23,7 +23,7 @@ pub trait GPURayTracingDeviceProvider {
     &self,
     desc: &GPURaytracingPipelineBuilder,
   ) -> Box<dyn GPURaytracingPipelineProvider>;
-  fn create_sbt(&self, desc: &GPURaytracingPipelineBuilder) -> Box<dyn ShaderBindingTableProvider>;
+  fn create_sbt(&self) -> Box<dyn ShaderBindingTableProvider>;
 }
 
 pub struct HitGroupShaderRecord {
@@ -38,6 +38,59 @@ pub trait ShaderBindingTableProvider {
   fn config_hit_group(&mut self, mesh_idx: u32, hit_group: HitGroupShaderRecord);
   fn config_missing(&mut self, ray_ty_idx: u32, s: ShaderHandle);
   fn access_impl(&mut self) -> &mut dyn Any;
+}
+
+pub trait GPUAccelerationStructureInstanceBuilder {
+  fn create_top_level_acceleration_structure(
+    &self,
+    source: &[TopLevelAccelerationStructureSourceInstance],
+  ) -> Box<dyn GPUAccelerationStructureInstanceProvider>;
+
+  fn delete_top_level_acceleration_structure(
+    &self,
+    id: Box<dyn GPUAccelerationStructureInstanceProvider>,
+  );
+
+  fn create_bottom_level_acceleration_structure_by_triangles(
+    &self,
+    positions: &[Vec3<f32>],
+    indices: &[u32],
+  ) -> BottomLevelAccelerationStructureHandle;
+
+  fn create_bottom_level_acceleration_structure_by_aabbs(
+    &self,
+    aabbs: &[[f32; 6]],
+  ) -> BottomLevelAccelerationStructureHandle;
+
+  fn delete_bottom_level_acceleration_structure(&self, id: BottomLevelAccelerationStructureHandle);
+}
+pub struct BottomLevelAccelerationStructureHandle(pub u32);
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub struct TopLevelAccelerationStructureSourceInstance {
+  pub transform: Mat4<f32>,
+  pub instance_custom_index: u32,
+  pub mask: u32,
+  pub instance_shader_binding_table_record_offset: u32,
+  pub flags: u32,
+  pub acceleration_structure_handle: u64,
+}
+
+pub trait GPUAccelerationStructureInstanceProvider {
+  fn access_impl(&self) -> &dyn Any;
+}
+
+// the below are specific for compute shader based impl.
+// todo, move the code into compute shader based impl.
+
+pub trait GPUAccelerationStructureInstance {
+  fn build_shader(
+    &self,
+    compute_cx: &mut ShaderComputePipelineBuilder,
+  ) -> Box<dyn GPUAccelerationStructureInvocationTraversable>;
+  fn bind_pass(&self, pass: &mut GPUComputePass);
+
+  fn handle(&self) -> u32;
 }
 
 pub trait GPUAccelerationStructureInvocationTraversable {
@@ -68,43 +121,4 @@ pub trait IntersectionReporter {
 pub struct DeviceOption<T> {
   pub is_some: Node<bool>,
   pub payload: T,
-}
-
-pub trait GPUAccelerationStructureInstance {
-  fn build_shader(
-    &self,
-    compute_cx: &mut ShaderComputePipelineBuilder,
-  ) -> Box<dyn GPUAccelerationStructureInvocationTraversable>;
-  fn bind_pass(&self, pass: &mut GPUComputePass);
-
-  fn handle(&self) -> u32;
-}
-
-pub trait GPUAccelerationStructureInstanceBuilder {
-  fn create_top_level_acceleration_structure(
-    &self,
-    source: &[TopLevelAccelerationStructureSourceInstance],
-  ) -> Box<dyn GPUAccelerationStructureInstance>;
-
-  fn delete_top_level_acceleration_structure(&self, id: Box<dyn GPUAccelerationStructureInstance>);
-
-  fn create_bottom_level_acceleration_structure_by_triangles(
-    &self,
-    positions: &[Vec3<f32>],
-    indices: &[u32],
-  ) -> u32;
-
-  fn create_bottom_level_acceleration_structure_by_aabbs(&self, aabbs: &[[f32; 6]]) -> u32;
-
-  fn delete_bottom_level_acceleration_structure(&self, id: u32);
-}
-
-#[derive(Clone, Copy, PartialEq, Debug)]
-pub struct TopLevelAccelerationStructureSourceInstance {
-  pub transform: Mat4<f32>,
-  pub instance_custom_index: u32,
-  pub mask: u32,
-  pub instance_shader_binding_table_record_offset: u32,
-  pub flags: u32,
-  pub acceleration_structure_handle: u64,
 }
