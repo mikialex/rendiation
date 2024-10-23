f029635d340b9542b2fcb287e51e582a52c54932
mikialex <18516340862@163.com>
2024-10-23 15:46:12 +0800

    fix tracer payload readback bumper not reset

--- shader/ray-tracing/src/backend/wavefront_compute/pipeline.rs
+++ shader/ray-tracing/src/backend/wavefront_compute/pipeline.rs
@@ -7,6 +7,7 @@ pub struct GPUWaveFrontComputeRaytracingBakedPipeline {
 
 pub struct GPUWaveFrontComputeRaytracingBakedPipelineInner {
   pub(crate) graph: DeviceTaskGraphExecutor,
+  pub(crate) tracer_read_back_bumper: Arc<RwLock<DeviceBumpAllocationInstance<u32>>>,
   pub(crate) target_sbt_buffer: StorageBufferReadOnlyDataView<u32>,
 }
 
@@ -62,17 +63,21 @@ impl GPUWaveFrontComputeRaytracingBakedPipelineInner {
     let target_sbt_buffer = StorageBufferReadOnlyDataView::create(device, &0);
 
     let payload_u32_len = init_size * 2 * (payload_max_u32_count as usize);
+    let payload_bumper = Arc::new(RwLock::new(DeviceBumpAllocationInstance::new(
+      payload_u32_len,
+      device,
+    )));
+
+    let payload_read_back_bumper = Arc::new(RwLock::new(DeviceBumpAllocationInstance::new(
+      payload_u32_len,
+      device,
+    )));
+
     let tracer_task = TraceTaskImpl {
       tlas_sys,
       sbt_sys,
-      payload_bumper: Arc::new(RwLock::new(DeviceBumpAllocationInstance::new(
-        payload_u32_len,
-        device,
-      ))),
-      payload_read_back_bumper: Arc::new(RwLock::new(DeviceBumpAllocationInstance::new(
-        payload_u32_len,
-        device,
-      ))),
+      payload_bumper: payload_bumper.clone(),
+      payload_read_back_bumper: payload_read_back_bumper.clone(),
       ray_info_bumper: DeviceBumpAllocationInstance::new(init_size * 2, device),
       info: Arc::new(info),
       current_sbt: target_sbt_buffer.clone(),
@@ -130,6 +135,7 @@ impl GPUWaveFrontComputeRaytracingBakedPipelineInner {
     GPUWaveFrontComputeRaytracingBakedPipelineInner {
       graph: executor,
       target_sbt_buffer,
+      tracer_read_back_bumper: payload_read_back_bumper,
     }
   }
 }
