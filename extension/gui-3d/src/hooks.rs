6c8bba288174b3440b60ef75cdc10adb6fe6b4eb
mikialex <18516340862@163.com>
2025-03-29 18:20:03 +0800

    try

--- extension/gui-3d/src/hooks.rs
+++ extension/gui-3d/src/hooks.rs
@@ -5,9 +5,11 @@ use crate::*;
 pub struct UICx<'a> {
   writer: &'a mut SceneWriter,
   scope: Vec<&'static Location<'static>>,
-  memory: Vec<Box<dyn Any>>,
+  memory: StateMemory,
+  memory_visit_stack: Vec<usize>,
   pub event: Option<UIEventStageCx<'a>>,
   pub view_writer: Option<&'a mut SceneWriter>,
+  pub current_parent: Option<EntityHandle<SceneNodeEntity>>,
   pub dyn_cx: &'a mut DynCx,
 }
 
@@ -16,10 +18,57 @@ pub struct UIEventStageCx<'a> {
   pub interaction_cx: &'a Interaction3dCtx,
 }
 
+struct StateCache<T> {
+  state: T,
+  cleanup: Option<fn(&mut T, &mut UICx)>,
+}
+
+struct StateMemory {
+  location: &'static Location<'static>,
+  memories: Vec<SubState>,
+}
+
+impl StateMemory {
+  pub fn clean_up(&mut self, cx: &mut UICx) {
+    for m in &mut self.memories {
+      match m {
+        SubState::State(s) => {
+          todo!()
+          //     let s = s.downcast_mut().unwrap();
+          //   if let Some(f) = &s.cleanup {
+          //     f(s.downcast_mut().unwrap(), cx)
+          //   }
+        }
+        SubState::SubTree(m) => {
+          m.clean_up(cx);
+        }
+      }
+    }
+  }
+}
+
+enum SubState {
+  State(Box<dyn Any>),
+  SubTree(StateMemory),
+}
+
 impl UICx<'_> {
+  fn get_next_memory(&mut self) -> Option<&mut SubState> {
+    let mut m = None;
+    // for i in 0..self.memory_visit_stack.len() {
+
+    //   m = Some(&mut m.memories[self.memory_visit_stack[i]]);
+    // }
+    m
+  }
+
   #[track_caller]
   pub fn scoped<R>(&mut self, f: impl FnOnce(&mut Self) -> R) -> R {
     self.scope.push(Location::caller());
+
+    let next_memory = self.get_next_memory();
+
+    self.memory_visit_stack.push(0);
     let r = f(self);
     self.scope.pop();
     r
@@ -29,7 +78,15 @@ impl UICx<'_> {
     todo!()
   }
 
-  pub fn use_state_init<'a, T: Sized>(&mut self, init: impl FnOnce(&mut DynCx) -> T) -> &'a mut T {
+  pub fn use_state_by<'a, T: Sized>(&mut self, default: T) -> &'a mut T {
+    todo!()
+  }
+
+  pub fn use_state_init<'a, T: Sized, D: FnOnce(&mut T, &mut Self)>(
+    &mut self,
+    init: impl FnOnce(&mut Self) -> (T, D),
+  ) -> &'a mut T {
+    // let current_memory =
     todo!()
   }
 }
