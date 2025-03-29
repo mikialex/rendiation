e5cd251c510b418e746eb618a8ccb60bff8f2abe
mikialex <18516340862@163.com>
2025-03-29 13:06:08 +0800

    prototyping

---
+++ extension/gui-3d/src/hooks.rs
@@ -0,0 +1,37 @@
+use std::{any::Any, panic::Location};
+
+use crate::*;
+
+pub struct UICx<'a> {
+  writer: &'a mut SceneWriter,
+  scope: Vec<&'static Location<'static>>,
+  memory: Vec<Box<dyn Any>>,
+}
+
+impl UICx<'_> {
+  #[track_caller]
+  pub fn scoped<R>(&mut self, f: impl FnOnce(&mut Self) -> R) -> R {
+    self.scope.push(Location::caller());
+    let r = f(self);
+    self.scope.pop();
+    r
+  }
+
+  pub fn use_state<'a, T: Sized>(&mut self) -> &'a mut T {
+    todo!()
+  }
+
+  pub fn use_state_init<'a, T: Sized>(&mut self, init: impl FnOnce(&mut DynCx) -> T) -> &'a mut T {
+    todo!()
+  }
+}
+
+#[track_caller]
+pub fn group(cx: &mut UICx, children: impl FnOnce(&mut UICx)) {
+  cx.scoped(|cx| {
+    let a = cx.use_state::<u32>();
+    let b = cx.use_state::<u32>();
+    // let node = use_effect(cx, |cx| cx.writer.node_writer.new_entity());
+    //
+  });
+}
