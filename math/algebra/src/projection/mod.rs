af9c8ce5c1600bf3f68d82a8f93772c6f59b30de
mikialex <miikiialexx@gmail.com>
2025-12-18 13:52:24 +0800

    impl pixels_per_unit for mat

--- math/algebra/src/projection/mod.rs
+++ math/algebra/src/projection/mod.rs
@@ -43,4 +43,41 @@ impl<T: Scalar> Mat4<T> {
       self.get_near_far_assume_orthographic()
     }
   }
+
+  /// Calculate how many screen pixel match one world unit at given distance.
+  ///
+  /// If the matrix originated from the common perspective, a better implementation can be used.
+  pub fn pixels_per_unit(&self, inverse_of_self: Self, distance: T, view_height_in_pixel: T) -> T {
+    let z_in_ndc = *self * Vec3::new(T::zero(), T::zero(), distance);
+    let z_in_ndc = z_in_ndc.z;
+    let ndc_top = inverse_of_self * Vec3::new(T::zero(), T::one(), z_in_ndc);
+    let ndc_bottom = inverse_of_self * Vec3::new(T::zero(), -T::one(), z_in_ndc);
+
+    let real_height = ndc_top.distance_to(ndc_bottom);
+    view_height_in_pixel / real_height
+  }
+}
+
+#[test]
+fn test_pixel_per_unit() {
+  let distance = 20.;
+  let view_height_in_pixel = 100.;
+
+  let p = PerspectiveProjection::<f32>::default();
+  let mat = p.compute_projection_mat(&OpenGLxNDC);
+  let inv_mat = mat.inverse_or_identity();
+
+  let mat_result = mat.pixels_per_unit(inv_mat, distance, view_height_in_pixel);
+  let result = p.pixels_per_unit(distance, view_height_in_pixel);
+
+  assert!(mat_result - result < 0.001);
+
+  let p = OrthographicProjection::<f32>::default();
+  let mat = p.compute_projection_mat(&OpenGLxNDC);
+  let inv_mat = mat.inverse_or_identity();
+
+  let mat_result = mat.pixels_per_unit(inv_mat, distance, view_height_in_pixel);
+  let result = p.pixels_per_unit(distance, view_height_in_pixel);
+
+  assert!(mat_result - result < 0.001);
 }
