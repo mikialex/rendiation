5e05361d524cc68299aff6449e81439d0d0a769d
mikialex <miikiialexx@gmail.com>
2026-06-25 13:29:31 +0800

    fix selection set change not trigger render change, update dev guide backlog

--- application/viewer-content/src/selection_states.rs
+++ application/viewer-content/src/selection_states.rs
@@ -1,5 +1,8 @@
+use futures::task::AtomicWaker;
+
 use crate::*;
 
+// todo, we should put this into db
 #[derive(Default)]
 pub struct ViewerSelectionStates {
   pub selected_model: ViewerModelSelectionSet,
@@ -11,9 +14,14 @@ pub struct ViewerSelectionStates {
 #[derive(Default)]
 pub struct ViewerModelSelectionSet {
   selected_models: FastHashSet<EntityHandle<SceneModelEntity>>,
+  waker: AtomicWaker,
 }
 
 impl ViewerModelSelectionSet {
+  pub fn register(&self, waker: &Waker) {
+    self.waker.register(waker);
+  }
+
   pub fn iter_selected(
     &self,
   ) -> impl Iterator<Item = EntityHandle<SceneModelEntity>> + Clone + 'static {
@@ -26,7 +34,11 @@ impl ViewerModelSelectionSet {
       .into_iter()
   }
   pub fn remove_select_if(&mut self, f: impl Fn(EntityHandle<SceneModelEntity>) -> bool) {
+    let len = self.selected_models.len();
     self.selected_models.retain(|m| !f(*m));
+    if len != self.selected_models.len() {
+      self.waker.wake();
+    }
   }
 
   pub fn has_selected(&self) -> bool {
@@ -35,10 +47,15 @@ impl ViewerModelSelectionSet {
 
   pub fn add_select(&mut self, model: EntityHandle<SceneModelEntity>) {
     self.selected_models.insert(model);
+    self.waker.wake();
   }
 
   pub fn clear(&mut self) {
+    if self.selected_models.is_empty() {
+      return;
+    }
     self.selected_models.clear();
+    self.waker.wake();
   }
 
   pub fn if_single(&self) -> Option<EntityHandle<SceneModelEntity>> {
